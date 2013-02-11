1f1b6b15e072d8ef8e2479fa8dc03d5bf19ae0bc
Jamey Hicks <jamey.hicks@nokia.com>
2013-02-11 16:10:33 -0500

    added t_error to lexer

--- syntax.py
+++ syntax.py
@@ -180,6 +180,10 @@ t_TILDECARET = r'~^'
 
 t_ignore = ' \t'
 
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
 def t_VAR(t):
     r'`?[a-zA-Z_][$a-zA-Z0-9_]*'
     t.type = reserved.get(t.value,'VAR')    
