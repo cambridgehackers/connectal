c7b47ccfa98077f8f317cbdb64276c94e576c836
Jamey Hicks <jamey.hicks@nokia.com>
2013-02-12 11:31:42 -0500

    moved [de]capitalize() to util.py

--- util.py
+++ util.py
@@ -6,3 +6,9 @@ def createDirAndOpen(f, m):
         os.makedirs(d)
     return open(f, m)
 
+## for camelcase preservation
+def capitalize(s):
+    return '%s%s' % (s[0].upper(), s[1:])
+def decapitalize(s):
+    return '%s%s' % (s[0].lower(), s[1:])
+
