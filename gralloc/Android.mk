d6f60ead9c6888f5c385b082c0c361baf58d442f
Jamey Hicks <jamey.hicks@qrclab.com>
2013-06-21 15:51:55 -0400

    remove redundant copies of portal.h and portal.cpp

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -6,10 +6,10 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 LOCAL_SRC_FILES := 	\
-	portal.cpp HdmiDisplay.cpp gralloc.cpp mapper.cpp
+	../cpp/portal.cpp HdmiDisplay.cpp gralloc.cpp mapper.cpp
 
 LOCAL_MODULE_TAGS = optional
 LOCAL_MODULE := gralloc.portal
-LOCAL_CFLAGS:= -DLOG_TAG=\"gralloc\"
+LOCAL_CFLAGS:= -DLOG_TAG=\"gralloc\" -I$(LOCAL_PATH)/../cpp
 
 include $(BUILD_SHARED_LIBRARY)
