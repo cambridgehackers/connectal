14cf6e040bce3f5857daad2d30c93ba901eb1893
John Ankcorn <jca@alum.mit.edu>
2013-10-18 13:45:38 -0400

    added new interface

--- bsvgen.py
+++ bsvgen.py
@@ -27,7 +27,7 @@ import AxiDMA::*;
 
 '''
 
-exposedInterfaces = ['HDMI', 'LEDS', 'ImageonVitaSensor', 'ImageonSensorData', 'FmcImageonInterface', 'SpiPins']
+exposedInterfaces = ['HDMI', 'LEDS', 'ImageonVitaSensor', 'ImageonVSensor', 'ImageonSensorData', 'FmcImageonInterface', 'SpiPins']
 
 bsimTopTemplate='''
 import StmtFSM::*;
