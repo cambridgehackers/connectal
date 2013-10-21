98b1206db23e5962c949f756a0ad5a3497cbeec7
Jamey Hicks <jamey.hicks@qrclab.com>
2013-10-21 11:37:40 -0400

    updated testecho.cpp for 64bit

--- examples/echo/testecho.cpp
+++ examples/echo/testecho.cpp
@@ -8,11 +8,11 @@ CoreEchoRequest *echo = 0;
 class TestEchoIndications : public CoreEchoIndication
 {
     virtual void heard(unsigned long v) {
-        fprintf(stderr, "heard an echo: %d\n", v);
+        fprintf(stderr, "heard an echo: %ld\n", v);
 	echo->say2(v, 2*v);
     }
     virtual void heard2(unsigned long a, unsigned long b) {
-      fprintf(stderr, "heard an echo2: %d %d\n", a, b);
+      fprintf(stderr, "heard an echo2: %ld %ld\n", a, b);
       exit(0);
     }
 };
@@ -23,6 +23,10 @@ int main(int argc, const char **argv)
     int v = 42;
     fprintf(stderr, "Saying %d\n", v);
     echo->say(v);
+    echo->say(v*5);
+    echo->say(v*17);
+    echo->say(v*93);
+    echo->say2(v, v*3);
     echo->setLeds(9);
     portalExec(0);
 }
