092506c1367cdef95982c51b26e3b7fc44cd8e67
Jamey Hicks <jamey.hicks@qrclab.com>
2013-11-01 16:05:25 -0400

    removed manually created pcie examples

--- examples/pcie-echo/impact.cmd
+++
@@ -1,8 +0,0 @@
-
-
-setMode -bscan
-setCable -p auto
-addDevice -p 1 -file .build/kc705/fpga/mkEchoTop.bit
-program -p 1
-quit
-
--- examples/pcie-loadstore/impact.cmd
+++
@@ -1,8 +0,0 @@
-
-
-setMode -bscan
-setCable -p auto
-addDevice -p 1 -file .build/kc705/fpga/mkLoadStoreTop.bit
-program -p 1
-quit
-
