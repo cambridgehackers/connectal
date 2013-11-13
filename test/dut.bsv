b93ad622b2935b5bff22a0d777d53c2de285a96b
Myron King <myron.king@qrclab.com>
2013-11-13 06:51:39 -0500

    removing some dead code

--- test/dut.bsv
+++
@@ -1,5 +0,0 @@
-
-// Device Under Test
-interface DUT;
-    method ActionValue#(Bit#(32)) operate(Bit#(32) a, Bit#(32) b);
-endinterface
--- test/dut.h
+++
@@ -1,9 +0,0 @@
-class DUT {
-public:
-    static DUT *createDUT(const char *instanceName);
-    void operate ( unsigned int, unsigned int );
-private:
-    DUT(UshwInstance *);
-    ~DUT();
-    UshwInstance *p;
-};
