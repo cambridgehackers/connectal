585093156a964c3ed3d1956aa3041349bec79a49
John Ankcorn <jca@alum.mit.edu>
2013-11-15 15:15:19 -0500

    HdmiDisplay.cpp file now generated

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -6,7 +6,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 LOCAL_SRC_FILES := 	\
-	../cpp/portal.cpp HdmiDisplay.cpp gralloc.cpp mapper.cpp
+	../cpp/portal.cpp gralloc.cpp mapper.cpp
+#	../cpp/portal.cpp HdmiDisplay.cpp gralloc.cpp mapper.cpp
 
 LOCAL_MODULE_TAGS = optional
 LOCAL_MODULE := gralloc.portal
