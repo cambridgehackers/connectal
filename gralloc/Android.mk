6af61b83c005cd36b214393645a2a85dd63631b6
John Ankcorn <jca@alum.mit.edu>
2013-11-20 10:04:47 -0500

    changed default project directory name for hdmidisplay

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -6,10 +6,10 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 LOCAL_SRC_FILES := 	\
-	../cpp/portal.cpp ../xpsproj/jni/HdmiDisplay.cpp gralloc.cpp mapper.cpp
+	../cpp/portal.cpp ../hdmiproj/jni/HdmiDisplay.cpp gralloc.cpp mapper.cpp
 
 LOCAL_MODULE_TAGS = optional
 LOCAL_MODULE := gralloc.portal
-LOCAL_CFLAGS:= -DMMAP_HW -DLOG_TAG=\"gralloc\" -I$(LOCAL_PATH)/../cpp -I$(LOCAL_PATH)/.. -I$(LOCAL_PATH)/../xpsproj/jni
+LOCAL_CFLAGS:= -DMMAP_HW -DLOG_TAG=\"gralloc\" -I$(LOCAL_PATH)/../cpp -I$(LOCAL_PATH)/.. -I$(LOCAL_PATH)/../hdmiproj/jni
 
 include $(BUILD_SHARED_LIBRARY)
