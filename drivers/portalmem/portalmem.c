a0457185ccca38e75b7ab1a38d6537396d23d6c0
John Ankcorn <jca@alum.mit.edu>
2013-12-06 13:11:01 -0500

    cleanup

--- drivers/portalmem/portalmem.c
+++ drivers/portalmem/portalmem.c
@@ -30,32 +30,23 @@
 
 /**
  * struct pa_buffer - metadata for a particular buffer
- * @size:		size of the buffer
- * @priv_virt:		private data to the buffer representable as
- *			a void *
+ * @size:              size of the buffer
  * @lock:		protects the buffers cnt fields
  * @kmap_cnt:		number of times the buffer is mapped to the kernel
  * @vaddr:		the kenrel mapping if kmap_cnt is not zero
  * @sg_table:		the sg table for the buffer
  */
 struct pa_buffer {
-  size_t size;
-  void *priv_virt;
-  struct mutex lock;
-  int kmap_cnt;
-  void *vaddr;
+  size_t          size;
+  struct mutex    lock;
+  int             kmap_cnt;
+  void            *vaddr;
   struct sg_table *sg_table;
 };
 
 static struct miscdevice miscdev;
-static unsigned int high_order_gfp_flags = (GFP_HIGHUSER | __GFP_ZERO |
-	    __GFP_NOWARN | __GFP_NORETRY | __GFP_NO_KSWAPD) & ~__GFP_WAIT;
-static unsigned int low_order_gfp_flags  = (GFP_HIGHUSER | __GFP_ZERO |
-					    __GFP_NOWARN);
-static const unsigned int orders[] = {8, 4, 0};
 
-static void free_buffer_page(struct pa_buffer *buffer, 
-	    struct page *page, unsigned int order)
+static void free_buffer_page(struct page *page, unsigned int order)
 {
   // this is causing kernel panic on x86
   // i'll leave it commented out for now
@@ -64,109 +55,18 @@ static void free_buffer_page(struct pa_buffer *buffer,
 
 static int pa_buffer_free(struct pa_buffer *buffer)
 {
-  struct sg_table *table = buffer->priv_virt;
+  struct sg_table *table = buffer->sg_table;
   struct scatterlist *sg;
   LIST_HEAD(pages);
   int i;
   printk("PortalAlloc::pa_system_heap_free\n");
   for_each_sg(table->sgl, sg, table->nents, i){
-    free_buffer_page(buffer, sg_page(sg), get_order(sg->length));
+    free_buffer_page(sg_page(sg), get_order(sg->length));
   }
   sg_free_table(table);
   kfree(table);
   kfree(buffer);
   return 0;
-}
-
-static struct pa_buffer *pa_buffer_create(unsigned long len,
-					  unsigned long align)
-{
-  struct pa_buffer *buffer;
-  struct sg_table *table;
-  struct scatterlist *sg;
-  struct list_head pages;
-  struct page_info {
-    struct page *page;
-    unsigned long order;
-    struct list_head list;
-  } *info = NULL, *tmp_info;
-  unsigned int max_order = orders[0];
-  long size_remaining = PAGE_ALIGN(len);
-  struct page *page;
-  int i = 0;
-
-  buffer = kzalloc(sizeof(struct pa_buffer), GFP_KERNEL);
-  if (!buffer)
-    return ERR_PTR(-ENOMEM);
-  INIT_LIST_HEAD(&pages);
-  while (size_remaining > 0) {
-    int ordindex = 0;
-    info = NULL;
-    for (; ordindex < ARRAY_SIZE(orders); ordindex++) {
-      gfp_t gfp_flags = low_order_gfp_flags;
-      if (orders[ordindex] > 4)
-        gfp_flags = high_order_gfp_flags;
-      if (size_remaining < (PAGE_SIZE << orders[ordindex]) || max_order < orders[ordindex])
-        continue;
-      page = alloc_pages(gfp_flags, orders[ordindex]);
-      if (page) {
-        info = kmalloc(sizeof(*info), GFP_KERNEL);
-        info->page = page;
-        info->order = orders[ordindex];
-        list_add_tail(&info->list, &pages);
-        size_remaining -= (1 << info->order) * PAGE_SIZE;
-        max_order = info->order;
-        i++;
-        break;
-      }
-    }
-    if (!info)
-      break;
-  }
-  if (info) {
-    table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
-    if (table) {
-      int ret = sg_alloc_table(table, i, GFP_KERNEL);
-      if (!ret) {
-        sg = table->sgl;
-        list_for_each_entry_safe(info, tmp_info, &pages, list) {
-          struct page *page = info->page;
-          sg_set_page(sg, page, (1 << info->order) * PAGE_SIZE, 0);
-          sg = sg_next(sg);
-          list_del(&info->list);
-          kfree(info);
-        }
-        buffer->priv_virt = table;
-        buffer->size = len;
-        table = buffer->priv_virt;
-        if (IS_ERR_OR_NULL(table)) {
-          pa_buffer_free(buffer);
-          return ERR_PTR(PTR_ERR(table));
-        }
-        buffer->sg_table = table;
-        buffer->size = len;
-        mutex_init(&buffer->lock);
-        /* this will set up dma addresses for the sglist -- it is not
-           technically correct as per the dma api -- a specific
-           device isn't really taking ownership here.  However, in practice on
-           our systems the only dma_address space is physical addresses.
-           Additionally, we can't afford the overhead of invalidating every
-           allocation via dma_map_sg. The implicit contract here is that
-           memory is ready for dma, ie if it has a
-           cached mapping that mapping has been invalidated */
-        for_each_sg(buffer->sg_table->sgl, sg, buffer->sg_table->nents, i)
-          sg_dma_address(sg) = sg_phys(sg);
-        return buffer;
-      }
-      kfree(table);
-    }
-  }
-  list_for_each_entry(info, &pages, list) {
-    free_buffer_page(buffer, info->page, info->order);
-    kfree(info);
-  }
-  kfree(buffer);
-  return ERR_PTR(-ENOMEM);
 }
 
 /*
@@ -176,14 +76,11 @@ static struct pa_buffer *pa_buffer_create(unsigned long len,
 static struct sg_table *pa_dma_buf_map(struct dma_buf_attachment *attachment,
 				       enum dma_data_direction direction)
 {
-  struct dma_buf *dmabuf = attachment->dmabuf;
-  struct pa_buffer *buffer = dmabuf->priv;
-  return buffer->sg_table;
+  return ((struct pa_buffer *)attachment->dmabuf->priv)->sg_table;
 }
 
 static void pa_dma_buf_unmap(struct dma_buf_attachment *attachment,
-			     struct sg_table *table,
-			     enum dma_data_direction direction)
+	     struct sg_table *table, enum dma_data_direction direction)
 {
 }
 
@@ -199,7 +96,7 @@ static int pa_dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
   mutex_lock(&buffer->lock);
   /* now map it to userspace */
   {
-    struct sg_table *table = buffer->priv_virt;
+    struct sg_table *table = buffer->sg_table;
     unsigned long addr = vma->vm_start;
     unsigned long offset = vma->vm_pgoff * PAGE_SIZE;
 
@@ -243,7 +140,8 @@ static void pa_dma_buf_release(struct dma_buf *dmabuf)
 
 static void *pa_dma_buf_kmap(struct dma_buf *dmabuf, unsigned long offset)
 {
-  return ((struct pa_buffer *)dmabuf->priv)->vaddr + offset * PAGE_SIZE;
+  struct pa_buffer *buffer = dmabuf->priv;
+  return buffer->vaddr + offset * PAGE_SIZE;
 }
 
 static void pa_dma_buf_kunmap(struct dma_buf *dmabuf, unsigned long offset,
@@ -256,27 +154,24 @@ static int pa_dma_buf_begin_cpu_access(struct dma_buf *dmabuf, size_t start,
 {
   struct pa_buffer *buffer = dmabuf->priv;
   void *vaddr = NULL;
-  struct scatterlist *sg;
-  int i, j;
-  pgprot_t pgprot;
-  struct sg_table *table = buffer->priv_virt;
 
   mutex_lock(&buffer->lock);
-  if (buffer->kmap_cnt)
-    vaddr = buffer->vaddr;
-  else {
+  vaddr = buffer->vaddr;
+  if (!buffer->kmap_cnt) {
+    struct sg_table *table = buffer->sg_table;
     int npages = PAGE_ALIGN(buffer->size) / PAGE_SIZE;
     struct page **pages = vmalloc(sizeof(struct page *) * npages);
     struct page **tmp = pages;
     if (pages) {
-      pgprot = pgprot_writecombine(PAGE_KERNEL);
+      int i, j;
+      struct scatterlist *sg;
+      pgprot_t pgprot = pgprot_writecombine(PAGE_KERNEL);
       for_each_sg(table->sgl, sg, table->nents, i) {
         int npages_this_entry = PAGE_ALIGN(sg_dma_len(sg)) / PAGE_SIZE;
         struct page *page = sg_page(sg);
         BUG_ON(i >= npages);
-        for (j = 0; j < npages_this_entry; j++) {
+        for (j = 0; j < npages_this_entry; j++)
           *(tmp++) = page++;
-        }
       }
       vaddr = vmap(pages, npages, VM_MAP, pgprot);
       vfree(pages);
@@ -300,8 +195,7 @@ static void pa_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
   struct pa_buffer *buffer = dmabuf->priv;
 
   mutex_lock(&buffer->lock);
-  buffer->kmap_cnt--;
-  if (!buffer->kmap_cnt) {
+  if (!--buffer->kmap_cnt) {
     vunmap(buffer->vaddr);
     buffer->vaddr = NULL;
   }
@@ -320,6 +214,105 @@ static struct dma_buf_ops dma_buf_ops = {
   .kmap             = pa_dma_buf_kmap,
   .kunmap           = pa_dma_buf_kunmap,
 };
+
+static struct dma_buf *dmabuffer_create(unsigned long len,
+					  unsigned long align)
+{
+  static unsigned int high_order_gfp_flags = (GFP_HIGHUSER | __GFP_ZERO |
+	    __GFP_NOWARN | __GFP_NORETRY | __GFP_NO_KSWAPD) & ~__GFP_WAIT;
+  static unsigned int low_order_gfp_flags  = (GFP_HIGHUSER | __GFP_ZERO |
+					    __GFP_NOWARN);
+  static const unsigned int orders[] = {8, 4, 0};
+  struct pa_buffer *buffer;
+  struct sg_table *table;
+  struct scatterlist *sg;
+  struct list_head pages;
+  struct page_info {
+    struct page *page;
+    unsigned long order;
+    struct list_head list;
+  } *info = NULL, *tmp_info;
+  unsigned int max_order = orders[0];
+  long size_remaining = len;
+  int infocount = 0;
+
+  buffer = kzalloc(sizeof(struct pa_buffer), GFP_KERNEL);
+  if (!buffer)
+    return ERR_PTR(-ENOMEM);
+  table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
+  if (!table) {
+    kfree(buffer);
+    return ERR_PTR(-ENOMEM);
+  }
+  INIT_LIST_HEAD(&pages);
+  while (size_remaining > 0) {
+    int ordindex = 0;
+    info = NULL;
+    for (; ordindex < ARRAY_SIZE(orders); ordindex++) {
+      gfp_t gfp_flags = low_order_gfp_flags;
+      if (orders[ordindex] > 4)
+        gfp_flags = high_order_gfp_flags;
+      if (size_remaining >= (PAGE_SIZE << orders[ordindex])
+       && max_order >= orders[ordindex]) {
+        struct page *page = alloc_pages(gfp_flags, orders[ordindex]);
+        if (page) {
+          info = kmalloc(sizeof(*info), GFP_KERNEL);
+          info->page = page;
+          info->order = orders[ordindex];
+          list_add_tail(&info->list, &pages);
+          size_remaining -= (1 << info->order) * PAGE_SIZE;
+          max_order = info->order;
+          infocount++;
+          break;
+        }
+      }
+    }
+    if (!info)
+      break;
+  }
+  if (info) {
+    int ret = sg_alloc_table(table, infocount, GFP_KERNEL);
+    if (!ret) {
+      struct dma_buf *dmabuf;
+      sg = table->sgl;
+      list_for_each_entry_safe(info, tmp_info, &pages, list) {
+        struct page *page = info->page;
+        sg_set_page(sg, page, (1 << info->order) * PAGE_SIZE, 0);
+        sg = sg_next(sg);
+        list_del(&info->list);
+        kfree(info);
+      }
+      if (IS_ERR_OR_NULL(table)) {
+        pa_buffer_free(buffer);
+        return ERR_PTR(PTR_ERR(table));
+      }
+      buffer->sg_table = table;
+      buffer->size = len;
+      mutex_init(&buffer->lock);
+      /* this will set up dma addresses for the sglist -- it is not
+         technically correct as per the dma api -- a specific
+         device isn't really taking ownership here.  However, in practice on
+         our systems the only dma_address space is physical addresses.
+         Additionally, we can't afford the overhead of invalidating every
+         allocation via dma_map_sg. The implicit contract here is that
+         memory is ready for dma, ie if it has a
+         cached mapping that mapping has been invalidated */
+      for_each_sg(buffer->sg_table->sgl, sg, buffer->sg_table->nents, infocount)
+        sg_dma_address(sg) = sg_phys(sg);
+      dmabuf = dma_buf_export(buffer, &dma_buf_ops, len, O_RDWR);
+      if (IS_ERR(dmabuf))
+        pa_buffer_free(buffer);
+      return dmabuf;
+    }
+    kfree(table);
+  }
+  list_for_each_entry(info, &pages, list) {
+    free_buffer_page(info->page, info->order);
+    kfree(info);
+  }
+  kfree(buffer);
+  return ERR_PTR(-ENOMEM);
+}
 
 /*
  * driver file operations
@@ -355,30 +348,20 @@ static long pa_unlocked_ioctl(struct file *filep, unsigned int cmd, unsigned lon
   }
   case PA_ALLOC: {
     struct PortalAllocHeader header;
-    struct pa_buffer *buffer = NULL;
     size_t align = 4096;
     struct dma_buf *dmabuf;
     if (copy_from_user(&header, (void __user *)arg, sizeof(header)))
       return -EFAULT;
     printk("%s, header.size=%zd\n", __FUNCTION__, header.size);
-    header.size = round_up(header.size, align);
-    buffer = pa_buffer_create(PAGE_ALIGN(header.size), align);
-    if (!buffer)
-      return -ENODEV;
-    if (IS_ERR(buffer))
-      return PTR_ERR(buffer);
-    dmabuf = dma_buf_export(buffer, &dma_buf_ops, buffer->size, O_RDWR);
-    if (IS_ERR(dmabuf)) {
-      pa_buffer_free(buffer);
-      header.fd = PTR_ERR(dmabuf);
-    }
-    else {
-      header.fd = dma_buf_fd(dmabuf, O_CLOEXEC);
-      if (header.fd < 0)
-        dma_buf_put(dmabuf);
-      printk("pa_get_dma_buf %08lx %zd\n", (unsigned long)(dmabuf->file), dmabuf->file->f_count.counter);
-    }
-    header.numEntries = buffer->sg_table->nents;
+    header.size = PAGE_ALIGN(round_up(header.size, align));
+    dmabuf = dmabuffer_create(header.size, align);
+    if (IS_ERR(dmabuf))
+      return PTR_ERR(dmabuf);
+    printk("pa_get_dma_buf %p %zd\n", dmabuf->file, dmabuf->file->f_count.counter);
+    header.numEntries = ((struct pa_buffer *)dmabuf->priv)->sg_table->nents;
+    header.fd = dma_buf_fd(dmabuf, O_CLOEXEC);
+    if (header.fd < 0)
+      dma_buf_put(dmabuf);
     if (copy_to_user((void __user *)arg, &header, sizeof(header)))
       return -EFAULT;
     return 0;
@@ -386,8 +369,6 @@ static long pa_unlocked_ioctl(struct file *filep, unsigned int cmd, unsigned lon
   case PA_DMA_ADDRESSES: {
     struct PortalAllocHeader header;
     struct file *f;
-    struct dma_buf *dmabuf;
-    struct pa_buffer *pabuffer;
     struct sg_table *sgtable;
     struct scatterlist *sg;
     int i;
@@ -395,9 +376,7 @@ static long pa_unlocked_ioctl(struct file *filep, unsigned int cmd, unsigned lon
     if (copy_from_user(&header, (void __user *)arg, sizeof(header)))
       return -EFAULT;
     f = fget(header.fd);
-    dmabuf = f->private_data;
-    pabuffer = dmabuf->priv;
-    sgtable = pabuffer->sg_table;
+    sgtable = ((struct pa_buffer *)((struct dma_buf *)f->private_data)->priv)->sg_table;
     for_each_sg(sgtable->sgl, sg, sgtable->nents, i) {
       unsigned long p = sg_phys(sg);
       if (copy_to_user((void __user *)&(palloc->entries[i].dma_address), &(p), sizeof(p))
