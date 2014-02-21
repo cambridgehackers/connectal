6cd28c4db5f0cb9dc414d6317fd76806137353d9
John Ankcorn <john.ankcorn@qrclab.com>
2014-02-21 14:08:36 -0500

    fixed case when board not already present

--- scripts/pciescan.sh
+++ scripts/pciescan.sh
@@ -1,8 +1,10 @@
-#
+#!/bin/bash
 set -x
 # invalidate existing device (so that scan will look for new one)
 BLUEDEVICE=`lspci -d 1be7:b100 | sed -e "s/ .*//"`
-sudo sh -c "echo 1 >/sys/bus/pci/devices/0000:$BLUEDEVICE/remove"
+if [ "$BLUEDEVICE" != "" ]; then
+    sudo sh -c "echo 1 >/sys/bus/pci/devices/0000:$BLUEDEVICE/remove"
+fi
 #sudo sh -c "echo 1 >/sys/bus/pci/devices/0000:01:00.0/remove"
 # remove existing driver, since there is some bug in the 'remove'
 # function, causing the driver to become unmapped although
