a1ab74e4b6147ca9621e8a3de1ce1b531eea0160
John Ankcorn <john.ankcorn@qrclab.com>
2014-03-21 13:19:53 -0400

    updated script

--- jtag/run_trace.sh
+++ jtag/run_trace.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 set -x
 set -e
-openocd -f zedtrace.cfg 2>trace.xx
-sed -e"s/\(.\)\(...\)\(....\)/\1 \2 \3 /" <trace.xx  \
+openocd -f zedtrace.cfg 2>trace.xx.tempfile
+sed -e"s/\(.\)\(...\)\(....\)/\1 \2 \3 /" <trace.xx.tempfile  \
 | sed -e"s/^0/0 0/" -e"s/^1/0 1/" -e"s/^2/1 0/" -e"s/^3/1 1/" \
       -e"s/^4/2 0/" -e"s/^5/2 1/" -e"s/^6/3 0/" -e"s/^7/3 1/" \
-      -e"s/^8/4 0/" -e"s/^9/4 1/" -e"s/^A/5 0/" -e"s/^B/5 1/" >trace.yy
+      -e"s/^8/4 0/" -e"s/^9/4 1/" -e"s/^A/5 0/" -e"s/^B/5 1/" >trace.log
+rm -f trace.xx.tempfile
