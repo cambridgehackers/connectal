8496c3a17a480dd9f9f10fe6a2f128382174be16
Jamey Hicks <jamey.hicks@qrclab.com>
2014-04-14 17:08:51 -0400

    use a gradient pattern in the framebuffer
    Change-Id: I7dd453796c285d9f319997c2f567b42e3b4307b7

--- gralloc/testhdmidisplay.cpp
+++ gralloc/testhdmidisplay.cpp
@@ -52,21 +52,35 @@ int main(int argc, const char **argv)
     int status = poller->setClockFrequency(1, 160000000, 0);
     init_i2c_hdmi();
 
-    int fbsize = 720*480*4;
+    int lines = 1080;
+    int pixels = 1920;
+    int fbsize = lines*pixels*4;
     int err = dma->alloc(fbsize, &portalAlloc);
     int fd = portalAlloc->header.fd;
     int *ptr = (int*)mmap(0, fbsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
     fprintf(stderr, "Filling frame buffer ptr=%p... ", ptr);
-    for (int i = 0; i < fbsize/4; i++) {
-      ptr[i] = 0x00FF00FF;
+    for (int line = 0; line < lines; line++) {
+      for (int pixel = 0; pixel < pixels; pixel++) {
+	int i = line*pixels + pixel;
+	float red = (float)line / (float)lines;
+	//float blue = (float)pixel / (float)pixels;
+	float blue = 0.0;
+	int v = (int)(255*red) << 16 | (int)(255*blue);
+	ptr[i] = v;
+      }
     }
     fprintf(stderr, "done\n");
     dma->dCacheFlushInval(portalAlloc, ptr);
     fprintf(stderr, "calling dma->reference\n");
     unsigned int ref_srcAlloc = dma->reference(portalAlloc);
+    dma->show_mem_stats(ChannelType_Read);
     sleep(10);
     fprintf(stderr, "Starting frame buffer ref=%d...", ref_srcAlloc);
     device->startFrameBuffer0(ref_srcAlloc);
     fprintf(stderr, "done\n");
+    while (1) {
+      dma->show_mem_stats(ChannelType_Read);
+      sleep(1);
+    }
 }
