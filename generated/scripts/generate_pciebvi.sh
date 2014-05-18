3fd4dff8f1597da1149b8508847fb2ec804f28e3
John Ankcorn <jca@alum.mit.edu>
2014-05-18 17:46:45 -0400

    generate PCIEWRAPPER from files checked into import_components.git

--- generated/scripts/generate_pciebvi.sh
+++ generated/scripts/generate_pciebvi.sh
@@ -8,8 +8,13 @@ scripts/importbvi.py -o PCIEWRAPPER.bsv -I PcieWrap -P PcieWrap \
     -n pipe_userclk1_in \
     -n pipe_userclk2_in \
     -n pl_link_gen2_cap \
+    -c user_clk_out \
+    -r user_reset_out \
     -f cfg_aer -f cfg_ds -f cfg_err -f cfg_interrupt \
     -f cfg_mgmt -f cfg_msg -f cfg_pmcsr -f cfg_pm \
     -f cfg_root_control \
     -f pipe -f pl_link -f pci_exp -f pcie_drp \
-    xilinx/pcie_7x_v2_1/synth/pcie_7x_0.v
+    -p lanes \
+    ../../import_components/Xilinx/generated/kc705/pcie_7x_0/synth/pcie_7x_0.v
+
+#    xilinx/pcie_7x_v2_1/synth/pcie_7x_0.v
