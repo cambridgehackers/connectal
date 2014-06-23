33157adc32c4e4b38523aca1a5546ef2189ed98e
Jamey Hicks <jamey.hicks@qrclab.com>
2014-06-23 13:37:49 -0400

    added --ipdir option to xbsvgen
    Change-Id: I5cb411e133e4228f41764f6d3cd4d50d83791512

--- scripts/xbsv-synth-ip.tcl
+++ scripts/xbsv-synth-ip.tcl
@@ -14,23 +14,23 @@ proc xbsv_set_board_part {} {
 }
 
 proc xbsv_synth_ip {core_name core_version ip_name params} {
-    global xbsvdir boardname
+    global xbsvipdir boardname
 
     ## make sure we have a project configured for the correct board
     if [catch {current_project}] {
 	xbsv_set_board_part
     }
 
-    if [file exists $xbsvdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci] {
-	read_ip $xbsvdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci
+    if [file exists $xbsvipdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci] {
+	read_ip $xbsvipdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci
     }  else {
-        file mkdir $xbsvdir/generated/xilinx/$boardname
-        create_ip -name $core_name -version $core_version -vendor xilinx.com -library ip -module_name $ip_name -dir $xbsvdir/generated/xilinx/$boardname
+        file mkdir $xbsvipdir/generated/xilinx/$boardname
+        create_ip -name $core_name -version $core_version -vendor xilinx.com -library ip -module_name $ip_name -dir $xbsvipdir/generated/xilinx/$boardname
         set_property -dict $params [get_ips $ip_name]
         report_property [get_ips $ip_name]
-        generate_target all [get_files $xbsvdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci]
+        generate_target all [get_files $xbsvipdir/generated/xilinx/$boardname/$ip_name/$ip_name.xci]
     }
-    if [file exists $xbsvdir/generated/xilinx/$boardname/$ip_name/$ip_name.dcp] {
+    if [file exists $xbsvipdir/generated/xilinx/$boardname/$ip_name/$ip_name.dcp] {
     } else {
 	catch {
 	    synth_ip [get_ips $ip_name]
