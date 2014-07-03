a1db74460323e9ef4032bd8512a9b6143a053f7e
John Ankcorn <john.ankcorn@qrclab.com>
2014-07-03 14:58:29 -0400

    updated ocd scripts to reflect longer Axi trace buffer

--- jtag/run_trace.sh
+++ jtag/run_trace.sh
@@ -6,4 +6,4 @@ sed -e"s/\(.\)\(...\)\(....\)/\1 \2 \3 /" <trace.xx.tempfile  \
 | sed -e"s/^0/0 0/" -e"s/^1/0 1/" -e"s/^2/1 0/" -e"s/^3/1 1/" \
       -e"s/^4/2 0/" -e"s/^5/2 1/" -e"s/^6/3 0/" -e"s/^7/3 1/" \
       -e"s/^8/4 0/" -e"s/^9/4 1/" -e"s/^A/5 0/" -e"s/^B/5 1/" >trace.log
-rm -f trace.xx.tempfile
+#rm -f trace.xx.tempfile
