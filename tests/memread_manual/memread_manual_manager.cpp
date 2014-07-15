674dca5d075bc0c4bf8a038df54f30d4000bc418
John Ankcorn <jca@alum.mit.edu>
2014-07-15 14:51:30 -0400

    cleanup/copyright for memread_manual

--- tests/memread_manual/memread_manual_manager.cpp
+++ tests/memread_manual/memread_manual_manager.cpp
@@ -1,3 +1,24 @@
+/* Copyright (c) 2014 Quanta Research Cambridge, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <sys/mman.h>
 #include <string.h>
@@ -12,9 +33,7 @@
 #include <inttypes.h>
 
 #include "portal.h"
-#include "DmaConfigProxy.h"
 #include "GeneratedTypes.h" 
-#include "DmaIndicationWrapper.h"
 
 static int trace_memory;// = 1;
 
@@ -495,7 +514,7 @@ int main(int argc, const char **argv)
     size_t align = 4096;
     printk("%s, srcAlloc.size=%zd\n", __FUNCTION__, srcAlloc.size);
     srcAlloc.size = PAGE_ALIGN(round_up(srcAlloc.size, align));
-    struct dma_buf *dmabuf = dmabuffer_create(srcAlloc.size, align);
+    struct dma_buf *dmabuf = portalmem_dmabuffer_create(srcAlloc.size, align);
     if (IS_ERR(dmabuf))
       return PTR_ERR(dmabuf);
     printk("pa_get_dma_buf %p %zd\n", dmabuf->file, dmabuf->file->f_count.counter);
