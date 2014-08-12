810003e8a79f05b1cb4437650cc63236ee5cc609
John Ankcorn <jca@alum.mit.edu>
2014-08-12 08:58:36 -0400

    changed name of script in fpgamake

--- scripts/xbsv-synth-ip.tcl
+++ scripts/xbsv-synth-ip.tcl
@@ -1,6 +1,26 @@
-source "board.tcl"
-source "$xbsvdir/../fpgamake/tcl/fpgamake-synth-ip.tcl"
+# Copyright (c) 2014 Quanta Research Cambridge, Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+set scriptsdir [file dirname [info script] ]
+source "$scriptsdir/../../fpgamake/tcl/ipcore.tcl"
 
 proc xbsv_synth_ip {core_name core_version ip_name params} {
-   fpgamake_synth_ip $core_name $core_version $ip_name $params
+   fpgamake_ipcore $core_name $core_version $ip_name $params
 }
