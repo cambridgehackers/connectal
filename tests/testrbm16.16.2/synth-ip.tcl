5dc1507771042d8ff5e4d75ad3f8c23b2068f13e
Jamey Hicks <jamey.hicks@qrclab.com>
2014-08-22 16:58:50 -0400

    testrbm16.16.2
    Change-Id: I673f1ec63ca67ced2469c4d951edc5f07e86e716

---
+++ tests/testrbm16.16.2/synth-ip.tcl
@@ -0,0 +1,5 @@
+source "board.tcl"
+source "$xbsvdir/scripts/xbsv-synth-ip.tcl"
+
+xbsv_synth_ip floating_point 7.0 fp_add [list CONFIG.Axi_Optimize_Goal {Performance} CONFIG.Maximum_Latency {false} CONFIG.Has_ARESETN {true}]
+xbsv_synth_ip floating_point 7.0 fp_mul [list CONFIG.Operation_Type {Multiply} CONFIG.Axi_Optimize_Goal {Resources} CONFIG.Maximum_Latency {false} CONFIG.Has_ARESETN {true}]
