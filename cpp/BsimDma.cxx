e71c3be1650b2e194410ed297ed4a1e6049f0ee0
John Ankcorn <jca@alum.mit.edu>
2014-09-08 21:54:05 -0400

    cleanup

--- cpp/BsimDma.cxx
+++ cpp/BsimDma.cxx
@@ -21,70 +21,65 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
 #include <sys/mman.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-
-#include "portal.h"
 #include "sock_utils.h"
 
+#define MAX_DMA_PORTS    4
+#define MAX_DMA_IDS     32
 typedef struct {
     unsigned char *buffer;
     uint32_t buffer_len;
     int size_accum;
-} DMAINFO[32];
-static DMAINFO dma_info[4];
+} DMAINFO[MAX_DMA_IDS];
+static DMAINFO dma_info[MAX_DMA_PORTS];
 static int dma_trace;// = 1;
 
-
-
-
-#define NUKE_ID   \
-uint32_t id = pref>>5;      \
-pref -= id<<5; 
-
-
-extern "C" {
-  void write_pareff32(uint32_t pref, uint32_t offset, unsigned int data){
-    NUKE_ID;
+extern "C" void write_pareff32(uint32_t pref, uint32_t offset, unsigned int data)
+{
+    uint32_t id = pref>>5;
+    pref -= id<<5; 
     if (dma_trace)
       fprintf(stderr, "%s: %d %d %d\n", __FUNCTION__, id, pref, offset);
     *(unsigned int *)&dma_info[id][pref].buffer[offset] = data;
-  }
+}
 
-  unsigned int read_pareff32(uint32_t pref, uint32_t offset){
-    NUKE_ID;
+extern "C" unsigned int read_pareff32(uint32_t pref, uint32_t offset)
+{
+    uint32_t id = pref>>5;
+    pref -= id<<5; 
     if (dma_trace)
       fprintf(stderr, "%s: %d %d %d\n", __FUNCTION__, id, pref, offset);
     return *(unsigned int *)&dma_info[id][pref].buffer[offset];
-  }
+}
 
-  void write_pareff64(uint32_t pref, uint32_t offset, uint64_t data){
-    NUKE_ID;
+extern "C" void write_pareff64(uint32_t pref, uint32_t offset, uint64_t data)
+{
+    uint32_t id = pref>>5;
+    pref -= id<<5; 
     if (dma_trace)
       fprintf(stderr, "%s: %d %d %d\n", __FUNCTION__, id, pref, offset);
     *(uint64_t *)&dma_info[id][pref].buffer[offset] = data;
-  }
+}
 
-  uint64_t read_pareff64(uint32_t pref, uint32_t offset){
-    NUKE_ID;
+extern "C" uint64_t read_pareff64(uint32_t pref, uint32_t offset)
+{
+    uint32_t id = pref>>5;
+    pref -= id<<5; 
     if (dma_trace)
       fprintf(stderr, "%s: %d %d %d\n", __FUNCTION__, id, pref, offset);
     return *(uint64_t *)&dma_info[id][pref].buffer[offset];
-  }
+}
 
-  void pareff_init(uint32_t id, uint32_t pref, uint32_t size){
+extern "C" void pareff_init(uint32_t id, uint32_t pref, uint32_t size)
+{
     if (dma_trace)
-      fprintf(stderr, "BsimDma::pareff id=%d pref=%d, size=%08x size_accum=%08x\n", id, pref, size, dma_info[id][pref].size_accum);
-    assert(pref < 32);
+      fprintf(stderr, "pareff_init: id=%d pref=%d, size=%08x size_accum=%08x\n", id, pref, size, dma_info[id][pref].size_accum);
+    assert(pref < MAX_DMA_IDS);
     dma_info[id][pref].size_accum += size;
     if(size == 0){
       int fd;
@@ -92,12 +87,11 @@ extern "C" {
       dma_info[id][pref].buffer = (unsigned char *)mmap(0,
           dma_info[id][pref].size_accum, PROT_WRITE|PROT_WRITE|PROT_EXEC, MAP_SHARED, fd, 0);
       if (dma_info[id][pref].buffer == MAP_FAILED) {
-	fprintf(stderr, "%s: mmap failed fd %x buffer %p size %x errno %d\n", __FUNCTION__, fd, dma_info[id][pref].buffer, size, errno);
+	fprintf(stderr, "pareff_init: mmap failed fd %x buffer %p size %x errno %d\n", fd, dma_info[id][pref].buffer, size, errno);
 	exit(-1);
       }
       dma_info[id][pref].buffer_len = dma_info[id][pref].size_accum;
     }
     if (dma_trace)
-      fprintf(stderr, "done\n");
-  }
+      fprintf(stderr, "pareff_init: done\n");
 }
