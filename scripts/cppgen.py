cf87050636cd1d2996558ce5cb6eaa517114ddff
John Ankcorn <jca@alum.mit.edu>
2014-10-23 15:44:31 -0400

    improve 'putFailed' check

--- scripts/cppgen.py
+++ scripts/cppgen.py
@@ -102,7 +102,7 @@ void %(namespace)s%(className)s_%(methodName)s (PortalInternal *p %(paramSeparat
     int __i = 50;
     while (!READL(p, temp_working_addr + 1) && __i-- > 0)
         ; /* busy wait a bit on 'fifo not full' */
-    if (!__i)
+    if (__i <= 0)
         PORTAL_PRINTF("putFailed: %(namespace)s%(className)s_%(methodName)s\\n");
 %(paramStructMarshall)s
 };
