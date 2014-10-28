33754097e8fbdf1572bb2050bd8734806abbb3a7
Jamey Hicks <jamey.hicks@qrclab.com>
2014-10-28 15:04:51 -0400

    record typedef params when parsing
    Change-Id: I837d29401e2b8ca7e7a963ae8e2a968e6d322b75

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -719,7 +719,13 @@ def p_typeDefBody(p):
 def p_typeDef(p):
     '''typeDef : TOKTYPEDEF typeDefBody VAR deriving SEMICOLON
                | TOKTYPEDEF typeDefBody VAR interfaceHashParams deriving SEMICOLON'''
-    p[0] = AST.TypeDef(p[2], p[3])
+    print 'p_typeDef', len(p)
+    if len(p) == 6:
+        print p[3], [], p[2]
+        p[0] = AST.TypeDef(p[2], p[3], [])
+    else:
+        print p[3], p[4], p[2]
+        p[0] = AST.TypeDef(p[2], p[3], p[4])
 
 
 def p_interfaceDef(p):
