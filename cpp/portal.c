9cda9bf2c1fa832a2661f5d258f6ceec3c501d7e
John Ankcorn <jca@alum.mit.edu>
2014-10-29 16:30:46 -0400

    add debug info comment

--- cpp/portal.c
+++ cpp/portal.c
@@ -93,6 +93,7 @@ void init_portal_internal(PortalInternal *pint, int id, PORTAL_INDFUNC handler, 
     snprintf(buff, sizeof(buff), "/dev/portal%d", pint->fpga_number);
 #ifdef BSIM   // BSIM version
     assert(id < MAX_BSIM_PORTAL_ID);
+//printf("[%s:%d] id %d handler %p fpga_number %d\n", __FUNCTION__, __LINE__, id, handler, bsim_fpga_map[id]);
     pint->fpga_number = bsim_fpga_map[id];
     pint->map_base = (volatile unsigned int*)(long)(pint->fpga_number * PORTAL_BASE_OFFSET);
     portalEnableInterrupts(pint, 1);
