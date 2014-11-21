91a55debdefc40711a249974bb018684034cc1c3
John Ankcorn <jca@alum.mit.edu>
2014-11-21 13:36:37 -0500

    simplify examples/bscan

--- examples/bscan/testbscan.cpp
+++ examples/bscan/testbscan.cpp
@@ -20,80 +20,42 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
-
+//#include <stdlib.h>
+//#include <semaphore.h>
 #include "BscanIndication.h"
 #include "BscanRequest.h"
 #include "GeneratedTypes.h"
 
-#define LOOP_COUNT 1000
-
-BscanRequestProxy *bscanRequestProxy = 0;
-
-typedef sem_t SEM_TYPE;
-#define SEMINIT(A) sem_init(A, 0, 0);
-#define SEMWAIT(A) sem_wait(A);
-#define SEMPOST(A) sem_post(A)
-
-static SEM_TYPE sem_bscan;
-
-PortalPoller *poller = 0;
-
-static void *pthread_worker(void *p)
-{
-    void *rc = NULL;
-    while (!rc) {
-        rc = poller->portalExec_poll(poller->portalExec_timeout);
-        if ((long) rc >= 0)
-            rc = poller->portalExec_event();
-    }
-    return rc;
-}
-static void init_thread()
-{
-    pthread_t threaddata;
-    SEMINIT(&sem_bscan);
-    pthread_create(&threaddata, NULL, &pthread_worker, (void*)poller);
-}
+static BscanRequestProxy *bscanRequestProxy = 0;
+static sem_t sem_bscan;
 
 class BscanIndication : public BscanIndicationWrapper
 {
 public:
     virtual void bscanGet(uint64_t v) {
-        printf("bscanGet: %"PRIx64"\n", v);
-        SEMPOST(&sem_bscan);
+        printf("bscanGet: %llx\n", (long long)v);
+        sem_post(&sem_bscan);
     }
-    BscanIndication(unsigned int id, PortalPoller *poller) : BscanIndicationWrapper(id, poller) {
-    }
+    BscanIndication(unsigned int id) : BscanIndicationWrapper(id) { }
 };
 
 int main(int argc, const char **argv)
 {
-    poller = new PortalPoller();
-    BscanIndication *bscanIndication = new BscanIndication(IfcNames_BscanIndication, poller);
-    // these use the default poller
+    BscanIndication *bscanIndication = new BscanIndication(IfcNames_BscanIndication);
     bscanRequestProxy = new BscanRequestProxy(IfcNames_BscanRequest);
 
-    poller->portalExec_init();
-    init_thread();
     portalExec_start();
-
     if (argc == 1) {
-    int v = 42;
-    printf("Bscan put %x\n", v);
-    for (int i = 0; i < 255; i++)
-      bscanRequestProxy->bscanPut(i, i*v);
+        int v = 42;
+        printf("Bscan put %x\n", v);
+        for (int i = 0; i < 255; i++)
+          bscanRequestProxy->bscanPut(i, i*v);
     }
     else if (argc == 2) {
-      bscanRequestProxy->bscanGet(atoll(argv[1]));
-      SEMWAIT(&sem_bscan);
+        bscanRequestProxy->bscanGet(atoll(argv[1]));
+        sem_wait(&sem_bscan);
     }
     else if (argc == 3)
-      bscanRequestProxy->bscanPut(atoll(argv[1]), atoll(argv[2]));
-
-    //print_dbg_requeste_intervals();
-    poller->portalExec_end();
+        bscanRequestProxy->bscanPut(atoll(argv[1]), atoll(argv[2]));
     return 0;
 }
