939086b7918b0473e1bdf5ef15dd257c8f33c3a8
Lawrence Stewart <stewart@qrclab.com>
2015-03-05 11:08:46 -0500

    switch to using fmcomms1 no os code for i2c

---
+++ examples/fmcomms1/fmci2c.c
@@ -0,0 +1,105 @@
+/* Copyright (c) 2014 Quanta Research Cambridge, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+/* copied from jca's i2chdmi.h */
+
+#include <linux/types.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include "/usr/include/linux/i2c.h"
+#include "/usr/include/linux/i2c-dev.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "i2czedboardandroid.c"
+
+
+
+int fmcomms1_get_version(int fd, int device, unsigned char *datap, int size)
+{
+  int status;
+  struct i2c_msg msgs[2];
+  unsigned char command = 1;
+  struct i2c_rdwr_ioctl_data arg;
+  msgs[0].addr = device;
+  msgs[0].flags = 0;
+  msgs[0].len = 1;
+  msgs[0].buf = &command;
+  msgs[1].addr = device;
+  msgs[1].flags = I2C_M_RD | I2C_M_RECV_LEN;
+  msgs[1].len = 33;
+  msgs[1].buf = datap;
+  arg.msgs = msgs;
+  arg.nmsgs = 2;
+  datap[0] = 1;
+  status = ioctl(fd, I2C_RDWR, &arg);
+  if (status != 0) {
+    fprintf(stderr, "[%s:%d]: ioctl I2C_RW write status=%d errno=%d [%s]\n", __FILE__, __LINE__, status, errno, strerror(errno));
+  }
+  msgs[0] = msgs[1];
+  status = ioctl(fd, I2C_RDWR, &arg);
+  if (status != 0) {
+    fprintf(stderr, "[%s:%d]: ioctl I2C_RW read status=%d errno=%d [%s]\n", __FILE__, __LINE__, status, errno, strerror(errno));
+  }
+  return status;
+}
+
+
+unsigned char version_data[256];
+void testi2c(char *i2cdevice, int deviceid)
+{
+   
+
+  int fd;
+    int i;
+    int res;
+    printf("i2cdevice is %s, deviceid is 0x%02x\n", i2cdevice, deviceid);
+    I2C_Init(i2cdevice, deviceid);
+
+
+    // start version query
+    memset(version_data, 0, 256);
+    for (i = 0; i < 256; i += 1) {
+      ret = I2C_Read(0x50, i, 16, &version_data[i]);
+      if (ret < 0) {
+	fprintf(stdout, "testi2c failed eeprom read at %d\n", i);
+	return;
+      }
+    }
+
+    printf ("getversion result %d\n", res);
+    for (i = 0; i < 256; i += 1) {
+      if ((i != 0) && ((i % 16) == 0)) printf("\n");
+      printf(" %2x[%c]", version_data[i],isalnum(version_data[i]) ? version_data[i]:" ");
+    }
+    printf("\n");
+    /*
+    memset(version_data, 0, 128);
+    res = fmcomms1_get_version(fd, deviceid, version_data, 128);
+    printf ("getversion result %d\n", res);
+    for (i = 0; i < 32; i += 1) {
+      if ((i != 0) && ((i % 16) == 0)) printf("\n");
+      printf(" %2x[%c]", version_data[i],isalnum(version_data[i]) ? version_data[i]:" ");
+    }
+    printf("\n");
+    */
+}
