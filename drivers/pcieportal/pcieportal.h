66675a06b9556f63a33cc73063d3a26505b06ff7
Jamey Hicks <jamey.hicks@qrclab.com>
2015-03-10 13:35:59 -0400

    no reason to expose pcieportal struct declarations to user space

--- drivers/pcieportal/pcieportal.h
+++ drivers/pcieportal/pcieportal.h
@@ -59,6 +59,7 @@ typedef unsigned int tTlpData[6];
 #define PCIE_SEND_FD         _IOR(BNOC_IOC_MAGIC,12,tSendFd*)
 #define PCIE_DEREFERENCE     _IOR(BNOC_IOC_MAGIC,13,int)
 
+#ifdef __KERNEL__
 /*
  * Per-device data
  */
@@ -78,9 +79,6 @@ struct pmentry {
 	struct list_head pmlist;
 };
 
-#ifndef __KERNEL__
-#define __iomem
-#endif
 typedef struct tBoard {
         void __iomem     *bar0io, *bar1io, *bar2io; /* bars */
         struct pci_dev   *pci_dev; /* pci device pointer */
@@ -94,7 +92,6 @@ typedef struct tBoard {
         unsigned int      open_count;
 } tBoard;
 
-#ifdef __KERNEL__
 extern tBoard* get_pcie_portal_descriptor(void);
 #endif
 
