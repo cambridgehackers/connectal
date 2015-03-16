ca5225447134ee2b5eaa4d3c5cab84dab1c039d7
John Ankcorn <jca@alum.mit.edu>
2015-03-15 20:09:50 -0400

    removed zynq build dependancy on vim (xxd)

---
+++ scripts/reorderbytes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# Copyright (c) 2014 Quanta Research Cambridge, Inc.
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import print_function
+import sys, array
+
+if __name__=='__main__':
+    if len(sys.argv) != 3:
+        print('reorderbytes <infile> <outfile>')
+        sys.exit(1)
+    filename = sys.argv[1]
+    if filename == '-':
+        infile = sys.stdin
+    else:
+        infile = open(filename)
+    readarr = array.array('I', infile.read())
+    readarr.byteswap()
+    open(sys.argv[2], 'w').write(readarr.tostring())
