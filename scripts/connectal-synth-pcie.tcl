3c574314e2147e665b01789f45f6432b6dbfccb3
Jamey Hicks <jamey.hicks@qrclab.com>
2015-03-20 12:32:24 -0400

    xilinx vc707 and kc705 switched to pcie_7x_0 configured for gen2 5.0GT/s x8

--- scripts/connectal-synth-pcie.tcl
+++ scripts/connectal-synth-pcie.tcl
@@ -5,16 +5,21 @@ source "$scriptsdir/../../fpgamake/tcl/ipcore.tcl"
 if {$need_pcie == "x7_gen1x8"} {
     set pcieversion {3.0}
     set maxlinkwidth {X8}
+    set maxinterfacewidth {128_bit}
+    set linkspeed {5.0_GT/s}
+    ##set linkspeed {2.5_GT/s}
     if {$boardname == {zc706}} {
 	set maxlinkwidth {X4}
+	set maxinterfacewidth {64_bit}
     }
     if {$boardname == {ac701}} {
 	set maxlinkwidth {X4}
+	set maxinterfacewidth {64_bit}
     }
     if {[version -short] == "2013.2"} {
 	set pcieversion {2.1}
     }
-    connectal_synth_ip pcie_7x $pcieversion pcie_7x_0 [list CONFIG.mode_selection {Advanced} CONFIG.ASPM_Optionality {true} CONFIG.Disable_Tx_ASPM_L0s {true} CONFIG.Buf_Opt_BMA {true} CONFIG.Bar0_64bit {true} CONFIG.Bar0_Size {16} CONFIG.Bar0_Scale {Kilobytes} CONFIG.Bar2_64bit {true} CONFIG.Bar2_Enabled {true} CONFIG.Bar2_Scale {Megabytes} CONFIG.Bar2_Size {1} CONFIG.Base_Class_Menu {Memory_controller} CONFIG.Device_ID {c100} CONFIG.IntX_Generation {false} CONFIG.MSI_Enabled {false} CONFIG.MSIx_Enabled {true} CONFIG.MSIx_PBA_Offset {1f0} CONFIG.MSIx_Table_Offset {200} CONFIG.MSIx_Table_Size {10} CONFIG.Maximum_Link_Width $maxlinkwidth CONFIG.Subsystem_ID {a705} CONFIG.Subsystem_Vendor_ID {1be7} CONFIG.Use_Class_Code_Lookup_Assistant {false} CONFIG.Vendor_ID {1be7} ]
+    connectal_synth_ip pcie_7x $pcieversion pcie_7x_0 [list CONFIG.mode_selection {Advanced} CONFIG.ASPM_Optionality {true} CONFIG.Disable_Tx_ASPM_L0s {true} CONFIG.Buf_Opt_BMA {true} CONFIG.Bar0_64bit {true} CONFIG.Bar0_Size {16} CONFIG.Bar0_Scale {Kilobytes} CONFIG.Bar2_64bit {true} CONFIG.Bar2_Enabled {true} CONFIG.Bar2_Scale {Megabytes} CONFIG.Bar2_Size {1} CONFIG.Base_Class_Menu {Memory_controller} CONFIG.Device_ID {c100} CONFIG.IntX_Generation {false} CONFIG.MSI_Enabled {false} CONFIG.MSIx_Enabled {true} CONFIG.MSIx_PBA_Offset {1f0} CONFIG.MSIx_Table_Offset {200} CONFIG.MSIx_Table_Size {10} CONFIG.Maximum_Link_Width $maxlinkwidth CONFIG.Subsystem_ID {a705} CONFIG.Subsystem_Vendor_ID {1be7} CONFIG.Use_Class_Code_Lookup_Assistant {false} CONFIG.Vendor_ID {1be7} CONFIG.Link_Speed $linkspeed CONFIG.Interface_Width $maxinterfacewidth CONFIG.en_ext_clk {false} CONFIG.PCIe_Debug_Ports {false} CONFIG.shared_logic_in_core {true}]
 # Description of MSIx_Table_Offset is in:
 # Xilinx/Vivado/2013.2/data/ip/xilinx/pcie_7x_v2_1/xgui/pcie_7x_v2_1.tcl
 # (it is byteoffset/8, expressed in hex)
