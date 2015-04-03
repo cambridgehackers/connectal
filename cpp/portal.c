26ec9af4f9d6b8a2ce38c3fc91bd4ee9cce2de00
Jamey Hicks <jamey.hicks@qrclab.com>
2015-04-03 09:28:30 -0400

    exit if fpgajtag did not return status 0

--- cpp/portal.c
+++ cpp/portal.c
@@ -174,6 +174,9 @@ static void init_portal_hw(void)
         int fd, status, len;
         waitpid(pid, &status, 0);
 #ifdef __arm__
+	fprintf(stderr, "subprocess pid %d completed status=%x %d\n", pid, status, WEXITSTATUS(status));
+	if (WEXITSTATUS(status) != 0)
+	  exit(-1);
         fd = open("/dev/connectal", O_RDONLY); /* scan the fpga directory */
         len = read(fd, &status, sizeof(status));
 printf("[%s:%d] fd %d len %d\n", __FUNCTION__, __LINE__, fd, len);
