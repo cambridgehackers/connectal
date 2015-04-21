5ccfb253b0426a3a72e6117521196e389d8c1d7a
Jamey Hicks <jamey.hicks@qrclab.com>
2015-04-21 10:07:56 -0400

    connected device proxy and indication

--- tests/ov7670/testcam.cpp
+++ tests/ov7670/testcam.cpp
@@ -1,6 +1,41 @@
+/* Copyright (c) 2014 Quanta Research Cambridge, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
+#include "Ov7670ControllerRequest.h"
+#include "Ov7670ControllerIndication.h"
+
+class Ov7670ControllerIndication : public Ov7670ControllerIndicationWrapper {
+public:
+  Ov7670ControllerIndication(unsigned int id) : Ov7670ControllerIndicationWrapper(id){}
+  virtual void probeResponse(uint8_t data) {
+    fprintf(stderr, "i2c response %02x\n", data);
+  }
+  virtual void vsync() {
+    fprintf(stderr, "vsync\n");
+  }
+};
 
 int main(int argc, const char **argv)
 {
+  Ov7670ControllerRequestProxy device(IfcNames_Ov7670ControllerRequestS2H);
+  Ov7670ControllerIndication deviceResponse(IfcNames_Ov7670ControllerIndicationH2S);
   return 0;
 }
