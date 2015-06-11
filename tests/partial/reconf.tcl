1de1a25383812a8846b674f55f65295ec6a1adb5
Jamey Hicks <jamey.hicks@gmail.com>
2015-06-11 12:25:11 -0400

    fixed naming problems

--- tests/partial/reconf.tcl
+++ tests/partial/reconf.tcl
@@ -1,9 +1,9 @@
 ##
 if [file exists zc702/Impl/Reconf/top-post-route.dcp] {
     open_checkpoint zc702/Impl/Reconf/top-post-route.dcp
-    update_design -cells */ipDriver -black_box
+    update_design -cells top/lEcho_bounce -black_box
     lock_design -level routing
-    read_checkpoint -cell */ipDriver zc702/Synth/mkBounce1/mkBounce1-synth.dcp
+    read_checkpoint -cell top/lEcho_bounce zc702/Synth/mkBounce1/mkBounce1-synth.dcp
     opt_design
     place_design
     phys_opt_design
@@ -13,12 +13,12 @@ if [file exists zc702/Impl/Reconf/top-post-route.dcp] {
     write_checkpoint -force zc702/Impl/Reconf/reconf-post-route.dcp
     write_bitstream -force zc702/Impl/Reconf/reconf.bit
 } else {
-    open_checkpoint zc702/Impl/TopDown/top-post-link.dcp
-    update_design -cells */ipDriver -black_box
+    open_checkpoint zc702/Synth/mkZynqTop/mkZynqTop-synth.dcp
+    read_xdc ../../constraints/xilinx/zc7z020clg484.xdc
     read_xdc floorplan.xdc
-    set_property RESET_AFTER_RECONFIG 1 [get_pblocks pblock_ipDriver]
-    set_propert HD.RECONFIGURABLE 1 [get_cells */ipDriver]
-    read_checkpoint -cell */ipDriver zc702/Synth/mkBounce1/mkBounce1-synth.dcp
+    set_property RESET_AFTER_RECONFIG 1 [get_pblocks pblock_lEcho_bounce]
+    set_propert HD.RECONFIGURABLE 1 [get_cells top/lEcho_bounce]
+    read_checkpoint -cell top/lEcho_bounce zc702/Synth/mkBounce1/mkBounce1-synth.dcp
     opt_design
     place_design
     phys_opt_design
