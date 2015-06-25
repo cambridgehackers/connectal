8ce4de49219dd54609fa9c65d73444fc4130ec2c
Jamey Hicks <jamey.hicks@gmail.com>
2015-06-25 14:22:31 -0400

    print out source and dest of timing failure

--- scripts/check-timing.py
+++ scripts/check-timing.py
@@ -4,6 +4,7 @@ import sys, re
 
 failed = 0
 for timingreport in sys.argv[1:]:
+    print_lines = 0
     for line in open(timingreport):
         re_no_clock = re.compile('\s*There are (\d+) register/latch pins with no clock(.*)')
         re_constant_clock = re.compile('\s*There are (\d+) .*constant_clock(.*)')
@@ -22,8 +23,15 @@ for timingreport in sys.argv[1:]:
         m = re_violated.match(line)
         if m and float(m.group(1)) >= 0.1:
             print '*** timing violation ***'
-            print line
+            print line[0:-1]
             failed = 1
+            print_lines = 4
+            continue
+        if print_lines:
+            print line[0:-1]
+            print_lines -= 1
+            if not print_lines:
+                print
 if failed:
     sys.exit(-11)
 
