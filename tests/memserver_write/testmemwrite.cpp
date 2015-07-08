daeb2b7af5945092c1f66d4efe478dc1adb1b1aa
Jamey Hicks <jamey.hicks@gmail.com>
2015-07-08 15:22:09 -0400

    include sys/mman.h for declaration of munmap()

--- tests/memserver_write/testmemwrite.cpp
+++ tests/memserver_write/testmemwrite.cpp
@@ -18,6 +18,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
+#include <sys/mman.h>
 #include <errno.h>
 #include "dmaManager.h"
 #include "MemwriteIndication.h"
