91bf8320f55bd3da128ea3832cb6585e4694dc2f
Jamey Hicks <jamey.hicks@gmail.com>
2015-09-21 11:08:11 -0400

    modified dma example to use per-thread polling of DMA controller channels

--- examples/dma/testdmacopy.cpp
+++ examples/dma/testdmacopy.cpp
@@ -31,72 +31,103 @@
 #include "DmaIndication.h"
 #include "DmaRequest.h"
 
+class DmaIndication;
+DmaManager *dma;
+static int proxyNames[] = { IfcNames_DmaRequestS2H0, IfcNames_DmaRequestS2H1 };
+static int wrapperNames[] = { IfcNames_DmaIndicationH2S0, IfcNames_DmaIndicationH2S1 };
+
+pthread_t threads[2];
+
+class Channel {
+  PortalPoller *poller;
+  DmaIndication *dmaIndication;
+  DmaRequestProxy *dmaRequest;
+  int channel;
+  int srcfd;
+  int dstfd;
+  char *srcbuffer;
+  char *dstbuffer;
+  int srcRef;
+  int dstRef;
+  int size;
+  volatile int waitCount;
+public:
+    Channel(int channel);
+    void run();
+    void post() { waitCount--; }
+    static void *threadfn(void *c);
+};
+
 class DmaIndication : public DmaIndicationWrapper
 {
+    Channel *channel;
     sem_t sem;
 public:
-    DmaIndication(unsigned int id) : DmaIndicationWrapper(id){
+    DmaIndication(unsigned int id, PortalPoller *poller = 0, Channel *channel = 0)
+	: DmaIndicationWrapper(id, poller), channel(channel) {
 	sem_init(&sem, 0, 0);
     }
 
     void readDone ( uint32_t sglId, uint32_t base, const uint8_t tag ) {
     fprintf(stderr, "[%s:%d] sglId=%d base=%08x tag=%d\n", __FUNCTION__, __LINE__, sglId, base, tag);
 	sem_post(&sem);
+	if (channel)
+	    channel->post();
     }
     void writeDone ( uint32_t sglId, uint32_t base, uint8_t tag ) {
     fprintf(stderr, "[%s:%d] sglId=%d base=%08x tag=%d\n", __FUNCTION__, __LINE__, sglId, base, tag);
 	sem_post(&sem);
+	if (channel)
+	    channel->post();
     }
     void wait() {
 	sem_wait(&sem);
     }
 };
 
-DmaManager *dma;
-  static int proxyNames[] = { IfcNames_DmaRequestS2H0, IfcNames_DmaRequestS2H1 };
-  static int wrapperNames[] = { IfcNames_DmaIndicationH2S0, IfcNames_DmaIndicationH2S1 };
-class Channel {
-  DmaIndication *dmaIndication;
-  DmaRequestProxy *dmaRequest;
-  int channel;
-  int srcAlloc;
-  int dstAlloc;
-  int srcRef;
-  int dstRef;
-  int size;
-public:
-  Channel(int channel) : channel(channel), size(1024*1024) {
-    dmaRequest    = new DmaRequestProxy(proxyNames[channel]);
-    dmaIndication = new DmaIndication(wrapperNames[channel]);
+Channel::Channel(int channel)
+  : poller(new PortalPoller(0)), channel(channel), size(1024*1024), waitCount(0)
+{
+    dmaRequest    = new DmaRequestProxy(proxyNames[channel], poller);
+    dmaIndication = new DmaIndication(wrapperNames[channel], poller, this);
     fprintf(stderr, "[%s:%d] channel %d allocating buffers\n", __FUNCTION__, __LINE__, channel);
-    srcAlloc = portalAlloc(size, 0);
-    dstAlloc = portalAlloc(size, 0);
-    srcRef = dma->reference(srcAlloc);
-    dstRef = dma->reference(dstAlloc);
+    srcfd = portalAlloc(size, 0);
+    dstfd = portalAlloc(size, 0);
+    srcbuffer = (char *)portalMmap(srcfd, size);
+    dstbuffer = (char *)portalMmap(dstfd, size);
+    srcRef = dma->reference(srcfd);
+    dstRef = dma->reference(dstfd);
   }
-  void run() {
+
+void Channel::run()
+{
     fprintf(stderr, "[%s:%d] channel %d requesting first dma\n", __FUNCTION__, __LINE__, channel);
     dmaRequest->read(srcRef, 0, size/2, 0);
+    waitCount++;
+
     dmaRequest->write(dstRef, 0, size/2, 1);
+    waitCount++;
+
     fprintf(stderr, "[%s:%d] channel %d requesting second dma\n", __FUNCTION__, __LINE__, channel);
     dmaRequest->read(srcRef, size/2, size/2, 2);
+    waitCount++;
+
     dmaRequest->write(dstRef, size/2, size/2, 3);
+    waitCount++;
+
     fprintf(stderr, "[%s:%d] channel %d waiting for responses\n", __FUNCTION__, __LINE__, channel);
-    dmaIndication->wait();
-    dmaIndication->wait();
-    dmaIndication->wait();
-    dmaIndication->wait();
-  }
-  static void *threadfn(void *c) {
+    // poll
+    while (waitCount > 0) {
+	poller->event();
+    }
+}
+
+void *Channel::threadfn(void *c)
+{
     Channel *channelp = (Channel *)c;
     channelp->run();
     return 0;
-  }
-};
-
-
-
-pthread_t threads[2];
+}
 
 int main(int argc, const char **argv)
 {
