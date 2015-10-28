85745e70060d8962d388e5a8156f45620c34c66c
Jamey Hicks <jamey.hicks@gmail.com>
2015-10-28 11:33:24 -0400

    use default poller thread in xsim and verilator so main thread can focus on verilog simulation

--- cpp/XsimTop.cpp
+++ cpp/XsimTop.cpp
@@ -21,24 +21,46 @@
 #include <queue>
 #include <XsimMsgRequest.h>
 #include <XsimMsgIndication.h>
+#include <pthread.h>
 
 static int trace_xsimtop;// = 1;
 
 class XsimMsgRequest : public XsimMsgRequestWrapper {
-public:
+  pthread_mutex_t mutex;
   std::queue<uint32_t> sinkbeats[16];
-
-  XsimMsgRequest(int id, PortalTransportFunctions *item, void *param, PortalPoller *poller = 0):XsimMsgRequestWrapper(id, item, param, poller){}
-  ~XsimMsgRequest() {}
+public:
+  XsimMsgRequest(int id, PortalTransportFunctions *item, void *param, PortalPoller *poller = 0):XsimMsgRequestWrapper(id, item, param, poller){
+    pthread_mutex_init(&mutex, 0);
+  }
+  ~XsimMsgRequest() {
+    pthread_mutex_destroy(&mutex);
+  }
   void msgSink ( const uint32_t portal, const uint32_t data ) {
       if (trace_xsimtop)
           fprintf(stderr, "XsimRX: portal %d data=%08x\n", portal, data);
+      pthread_mutex_lock(&mutex);
       sinkbeats[portal].push(data);
+      pthread_mutex_unlock(&mutex);
   }
   void msgSinkFd ( const uint32_t portal, const uint32_t data ) {
       if (trace_xsimtop)
           fprintf(stderr, "XsimRXFD: portal %d data=%08x\n", portal, data);
+      pthread_mutex_lock(&mutex);
       sinkbeats[portal].push(data);
+      pthread_mutex_unlock(&mutex);
+  }
+  bool notEmpty(int portal) {
+      pthread_mutex_lock(&mutex);
+      bool notEmpty = sinkbeats[portal].size() > 0;
+      pthread_mutex_unlock(&mutex);
+      return notEmpty;
+  }
+  uint32_t get(int portal) {
+      pthread_mutex_lock(&mutex);
+      uint32_t data = sinkbeats[portal].front();
+      sinkbeats[portal].pop();
+      pthread_mutex_unlock(&mutex);
+      return data;
   }
 };
 
@@ -67,7 +89,9 @@ extern "C" void dpi_init()
 {
     if (trace_xsimtop)
       fprintf(stderr, "%s:%d &xsim_handler=%p\n", __FUNCTION__, __LINE__, &xsim_handler);
+#ifdef POLL_IN_DPI_POLL
     defaultPoller->stop();
+#endif
     mcommon = new Portal(0, 0, sizeof(uint32_t), portal_mux_handler, &xsim_handler, &transportSocketResp, NULL, NULL);
     PortalMuxParam param = {};
     param.pint = &mcommon->pint;
@@ -78,21 +102,22 @@ extern "C" void dpi_init()
 
 extern "C" void dpi_poll()
 {
+#ifdef POLL_IN_DPI_POLL
       void *rc = defaultPoller->pollFn(1);
       //fprintf(stderr, "%s:%d: rc=%ld\n", __FUNCTION__, __LINE__, (long)rc);
       if ((long)rc > 0)
 	  defaultPoller->event();
+#endif
 }
 
 extern "C" long long dpi_msgSink_beat(int portal)
 {
   long long result = 0xbadad7a;
   //if (trace_xsimtop) fprintf(stderr, "%s: portal %d rdy %d\n", __FUNCTION__, portal, (int)xsimRequest->sinkbeats[portal].size());
-  if (xsimRequest->sinkbeats[portal].size() > 0) {
-      uint32_t beat = xsimRequest->sinkbeats[portal].front();
+  if (xsimRequest->notEmpty(portal)) {
+      uint32_t beat = xsimRequest->get(portal);
       if (trace_xsimtop)
           fprintf(stderr, "%s: portal %d beat %08x\n", __FUNCTION__, portal, beat);
-      xsimRequest->sinkbeats[portal].pop();
       result = (1ll << 32) | beat;
   }
   return result;
