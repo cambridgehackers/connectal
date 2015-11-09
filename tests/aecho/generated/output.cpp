947f866f4231125a02b57e54686d0430c080e3a2
John Ankcorn <jca@alum.mit.edu>
2015-11-09 12:34:43 -0500

    updated tests/aecho for rename of test program

--- tests/aecho/generated/output.cpp
+++ tests/aecho/generated/output.cpp
@@ -4,45 +4,33 @@
 class l_class_OC_EchoTest echoTest;
 unsigned int stop_main_program;
 class l_class_OC_Module *_ZN6Module5firstE;
-
-
-//******************** vtables for Classes *******************
-unsigned char *_ZTVN8EchoTest5driveE[4] = { (unsigned char *)_ZN8EchoTest5drive3RDYEv, (unsigned char *)_ZN8EchoTest5drive3ENAEv };
-unsigned char *_ZTV4Rule[4] = {0 };
-unsigned char *_ZTVN4Echo7respond8respond2E[4] = { (unsigned char *)_ZN4Echo7respond8respond23RDYEv, (unsigned char *)_ZN4Echo7respond8respond23ENAEv };
-unsigned char *_ZTVN4Echo7respond8respond1E[4] = { (unsigned char *)_ZN4Echo7respond8respond13RDYEv, (unsigned char *)_ZN4Echo7respond8respond13ENAEv };
-unsigned char *_ZTV5Fifo1IiE[12] = {0 };
-unsigned char *_ZTV4FifoIiE[12] = {0 };
-typedef unsigned char **RuleVTab;//Rules:
-const RuleVTab ruleList[] = {
-    _ZTVN8EchoTest5driveE, _ZTVN4Echo7respond8respond2E, _ZTVN4Echo7respond8respond1E, NULL};
-//processing _ZN4Echo7respond8respond23ENAEv
-void _ZN4Echo7respond8respond23ENAEv(class l_class_OC_Echo_KD__KD_respond_KD__KD_respond2 *Vthis) {
-}
 //processing _ZN4Echo7respond8respond23RDYEv
-bool _ZN4Echo7respond8respond23RDYEv(class l_class_OC_Echo_KD__KD_respond_KD__KD_respond2 *Vthis) {
+bool _ZN4Echo7respond8respond23RDYEv(void) {
         return 1;
 }
-//processing _ZN4Echo7respond8respond13ENAEv
-void _ZN4Echo7respond8respond13ENAEv(class l_class_OC_Echo_KD__KD_respond_KD__KD_respond1 *Vthis) {
-        echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.deq();
-    unsigned int Vcall =     echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.first();
-        _ZN14EchoIndication4echoEi(Vcall);
+//processing _ZN4Echo7respond8respond23ENAEv
+void _ZN4Echo7respond8respond23ENAEv(void) {
 }
 //processing _ZN4Echo7respond8respond13RDYEv
-bool _ZN4Echo7respond8respond13RDYEv(class l_class_OC_Echo_KD__KD_respond_KD__KD_respond1 *Vthis) {
+bool _ZN4Echo7respond8respond13RDYEv(void) {
     bool Vtmp__1 =     echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.deq__RDY();
     bool Vtmp__2 =     echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.first__RDY();
         return (Vtmp__1 & Vtmp__2);
 }
-//processing _ZN8EchoTest5drive3ENAEv
-void _ZN8EchoTest5drive3ENAEv(class l_class_OC_EchoTest_KD__KD_drive *Vthis) {
-        echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.enq(22);
+//processing _ZN4Echo7respond8respond13ENAEv
+void _ZN4Echo7respond8respond13ENAEv(void) {
+        echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.deq();
+    unsigned int Vcall =     echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.first();
+        _ZN14EchoIndication4echoEi(Vcall);
 }
 //processing _ZN8EchoTest5drive3RDYEv
-bool _ZN8EchoTest5drive3RDYEv(class l_class_OC_EchoTest_KD__KD_drive *Vthis) {
+bool _ZN8EchoTest5drive3RDYEv(void) {
     bool Vtmp__1 =     echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.enq__RDY();
         return Vtmp__1;
+}
+//processing _ZN8EchoTest5drive3ENAEv
+void _ZN8EchoTest5drive3ENAEv(void) {
+        echoTest_ZZ_EchoTest_ZZ_echo_ZZ__ZZ_Echo_ZZ_fifo_ZZ__ZZ_Fifo1_int_.enq(22);
 }
 //processing _ZN14EchoIndication4echoEi
 void _ZN14EchoIndication4echoEi(unsigned int Vv) {
@@ -50,3 +38,12 @@ void _ZN14EchoIndication4echoEi(unsigned int Vv) {
         stop_main_program = 1;
 }
 //processing printf
+typedef struct {
+    bool (*RDY)(void);
+    void (*ENA)(void);
+    } RuleVTab;//Rules:
+const RuleVTab ruleList[] = {
+    {_ZN4Echo7respond8respond23RDYEv, _ZN4Echo7respond8respond23ENAEv},
+    {_ZN4Echo7respond8respond13RDYEv, _ZN4Echo7respond8respond13ENAEv},
+    {_ZN8EchoTest5drive3RDYEv, _ZN8EchoTest5drive3ENAEv},
+    {} };
