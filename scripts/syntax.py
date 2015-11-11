24225a14eab90deeeb254e3f1606d82724d52825
Jamey Hicks <jamey.hicks@gmail.com>
2015-11-11 09:37:46 -0500

    rewrite syntax.preprocess as iteration

--- scripts/syntax.py
+++ scripts/syntax.py
@@ -1002,24 +1002,39 @@ def p_package(p):
     '''package : beginPackage exportDecls importDecls packageStmts exportDecls endPackage'''
     p[0] = p[4]
 
-def preprocess(source, defs, bsvpath):
+def preprocess(sourcefilename, source, defs, bsvpath):
     stack = [(True,True)]
     def nexttok(s):
         k = re.search('\s', s)
-        sym = s[:k.start()]
-        s = s[k.end():]
-        return (sym, s)
-    def pp(s):
+        if k:
+            sym = s[:k.start()]
+            s = s[k.end():]
+            return (sym, s)
+        else:
+            return (s, '')
+    lines = source.splitlines()
+    outlines = []
+    while lines:
+        line = lines[0]
+        lines = lines[1:]
         cond  = stack[-1][0]
         valid = stack[-1][1]
-        i = re.search('\n[ \t]*`', s)
-        if i == None:
-            return s
+
+        commentStart = re.match('//', line)
+        if commentStart:
+            s = line[0:commentStart.start()]
+        else:
+            s = line
+        i = re.match('`', line)
+        if not i:
+            if valid:
+                outlines.append(line)
+            else:
+                outlines.append('//SKIPPED %s' % line)
+            continue
         pre = s[:i.end()-1]
         s = s[i.end():]
-        j = re.search('\s', s)
-        tok = s[:j.start()]
-        s = s[j.end():]
+        (tok, s) = nexttok(s)
         if tok == 'ifdef':
             (sym, s) = nexttok(s)
             new_cond = sym in defs
@@ -1046,39 +1061,38 @@ def preprocess(source, defs, bsvpath):
             stack.pop()
             valid = stack[-1][1]
         elif tok == 'define':
-            k = re.search('\n', s).start()
-            foo = re.search('\s',s).start()
-            sym = s[:min(k,foo)]
+            (sym, s) = nexttok(s)
             defs.append(sym)
-            s = s[k:]
         elif tok == 'include':
-            m = re.search('\s*\"([^\"]+)\"\s*', s)
+            m = re.search('"?([-_A-Za-z0-9.]+)"?', s)
+            if not m:
+                sys.stderr.write('syntax.preprocess %s: could not find file in line {%s}\n' % (sourcefilename, s))
             filename = m.group(1)
-            if valid:
-                inc = ''
-                for d in bsvpath:
-                    fn = os.path.join(d, filename)
-                    if os.path.exists(fn):
-                        inc = open(fn).read()
-                        break
-                s = '\\`include "%s"\n%s%s' % (filename, inc, s[m.end():])
-            else:
-                s = '\\`include "%s"\n%s%s' % (filename, '', s[m.end():])
+            inc = ''
+            for d in bsvpath:
+                fn = os.path.join(d, filename)
+                if os.path.exists(fn):
+                    inc = open(fn).read()
+                    break
+            if not inc:
+                sys.stderr.write('syntax.preprocess %s: did not find included file %s in path\n' % (sourcefilename, filename))
+            outlines.append('//`include "%s"' % filename)
+            lines.extend(inc.splitlines())
+            continue
         elif re.match('[A-Z][A-Za-z0-9_]*', tok):
             ## must be an undefined variable
-            sys.stderr.write('syntax.py: undefined preprocessor variable `%s\n' % tok)
+            sys.stderr.write('syntax.preprocess %s: preprocessor variable `%s\n' % (sourcefilename, tok))
         else:
-            print '%s: unhandled preprocessor token %s' % (globalfilename, tok)
+            sys.stderr.write('syntax.preprocess %s: unhandled preprocessor token %s\n' % (sourcefilename, tok))
             assert(tok in ['ifdef', 'ifndef', 'else', 'endif', 'define'])
-        prv = pre if valid and cond else '\n\n'
-        return prv+pp('\n'+s)
+        outlines.append('//PREPROCESSED: %s' % line)
 
-    return pp(source)
+    return '%s\n' % '\n'.join(outlines)
 
 def syntax_parse(argdata, inputfilename, bsvdefines, bsvpath):
     global globalfilename
     globalfilename = inputfilename
-    data = preprocess(argdata + '\n', bsvdefines, bsvpath)
+    data = preprocess(inputfilename, argdata + '\n', bsvdefines, bsvpath)
     lexer = lex.lex(errorlog=lex.NullLogger())
     parserdir=scripthome+'/syntax'
     if not os.path.isdir(parserdir):
