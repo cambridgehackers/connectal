4aba50fc2c609aef6221db2fc5fc1762a7d2e844
Jamey Hicks <jamey.hicks@gmail.com>
2016-01-18 14:31:15 -0500

    axieth.h

---
+++ tests/axieth/axieth.h
@@ -0,0 +1,22 @@
+#ifndef AXIETH_H
+#define AXIETH_H
+
+class AxiEthTestRequestProxy;
+class AxiEthTestIndication;
+
+class AxiEth {
+ public:
+  AxiEth();
+  ~AxiEth();
+  int irq ( const uint8_t newLevel );
+  void read(unsigned long offset, uint8_t *buf);
+  void write(unsigned long offset, const uint8_t *buf);
+ private:
+  AxiEthTestRequestProxy *request;
+  AxiEthTestIndication *indication;
+  bool didReset;
+
+  void maybeReset();
+};
+
+#endif
