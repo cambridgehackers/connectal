eba0d68437672bc26d75d3954927ff39d625ef2c
Jamey Hicks <jamey.hicks@gmail.com>
2016-04-21 11:52:55 -0400

    version 16.04.1

--- doc/library/source/conf.py
+++ doc/library/source/conf.py
@@ -66,7 +66,7 @@ googleanalytics_enabled = True
 # built documents.
 #
 # The short X.Y version.
-version = '16.02.1'
+version = '16.04.1'
 # The full version, including alpha/beta/rc tags.
 release = '16.02.1'
 
