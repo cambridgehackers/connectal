54161d906a6c8cb583ecc308069f558b70d150a5
Jamey Hicks <jamey.hicks@gmail.com>
2016-05-09 15:51:38 -0400

    dummy -> pythonTransport

--- examples/echopy/testecho.cpp
+++ examples/echopy/testecho.cpp
@@ -25,49 +25,49 @@
 
 static PyObject *callbackFunction;
 static PortalInternal erequest, eindication;
-static PortalInternal dummy;
+static PortalInternal pythonTransport;
 
 #define STUB \
 { \
     fprintf(stderr, "[%s:%d]\n", __FUNCTION__, __LINE__);	\
     exit(-1); \
 }
-static volatile unsigned int *dummyMAPCHANNELIND(struct PortalInternal *pint, unsigned int v)
+static volatile unsigned int *pythonTransportMAPCHANNELIND(struct PortalInternal *pint, unsigned int v)
 {
-    return &dummy.map_base[0];
+    return &pythonTransport.map_base[0];
 }
-static volatile unsigned int *dummyMAPCHANNELREQ(struct PortalInternal *pint, unsigned int v, unsigned int size)
+static volatile unsigned int *pythonTransportMAPCHANNELREQ(struct PortalInternal *pint, unsigned int v, unsigned int size)
 {
-    return &dummy.map_base[0];
+    return &pythonTransport.map_base[0];
 }
-static void dummySENDMSG(struct PortalInternal *pint, volatile unsigned int *buffer, unsigned int hdr, int sendFd)
+static void pythonTransportSENDMSG(struct PortalInternal *pint, volatile unsigned int *buffer, unsigned int hdr, int sendFd)
 {
 }
-static int dummyTRANSPORTINIT(struct PortalInternal *pint, void *param) STUB
-static unsigned int dummyREADWORD(struct PortalInternal *pint, volatile unsigned int **addr) STUB
-static void dummyWRITEWORD(struct PortalInternal *pint, volatile unsigned int **addr, unsigned int v) STUB
-static void dummyWRITEFDWORD(struct PortalInternal *pint, volatile unsigned int **addr, unsigned int v) STUB
-static int dummyRECVMSG(struct PortalInternal *pint, volatile unsigned int *buffer, int len, int *recvfd) STUB
-static int dummyBUSYWAIT(struct PortalInternal *pint, unsigned int v, const char *str) STUB
-static void dummyENABLEINT(struct PortalInternal *pint, int val) STUB
-static int dummyEVENT(struct PortalInternal *pint) STUB
-static int dummyNOTFULL(struct PortalInternal *pint, unsigned int v) STUB
+static int pythonTransportTRANSPORTINIT(struct PortalInternal *pint, void *param) STUB
+static unsigned int pythonTransportREADWORD(struct PortalInternal *pint, volatile unsigned int **addr) STUB
+static void pythonTransportWRITEWORD(struct PortalInternal *pint, volatile unsigned int **addr, unsigned int v) STUB
+static void pythonTransportWRITEFDWORD(struct PortalInternal *pint, volatile unsigned int **addr, unsigned int v) STUB
+static int pythonTransportRECVMSG(struct PortalInternal *pint, volatile unsigned int *buffer, int len, int *recvfd) STUB
+static int pythonTransportBUSYWAIT(struct PortalInternal *pint, unsigned int v, const char *str) STUB
+static void pythonTransportENABLEINT(struct PortalInternal *pint, int val) STUB
+static int pythonTransportEVENT(struct PortalInternal *pint) STUB
+static int pythonTransportNOTFULL(struct PortalInternal *pint, unsigned int v) STUB
 PortalTransportFunctions callbackTransport = {
-    dummyTRANSPORTINIT, dummyREADWORD, dummyWRITEWORD, dummyWRITEFDWORD,
-    dummyMAPCHANNELIND, dummyMAPCHANNELREQ, dummySENDMSG, dummyRECVMSG,
-    dummyBUSYWAIT, dummyENABLEINT, dummyEVENT, dummyNOTFULL};
+    pythonTransportTRANSPORTINIT, pythonTransportREADWORD, pythonTransportWRITEWORD, pythonTransportWRITEFDWORD,
+    pythonTransportMAPCHANNELIND, pythonTransportMAPCHANNELREQ, pythonTransportSENDMSG, pythonTransportRECVMSG,
+    pythonTransportBUSYWAIT, pythonTransportENABLEINT, pythonTransportEVENT, pythonTransportNOTFULL};
 
 static int heard_cb(struct PortalInternal *p,uint32_t v) {
-    EchoIndicationJson_heard (&dummy, v);
+    EchoIndicationJson_heard (&pythonTransport, v);
     PyGILState_STATE gstate = PyGILState_Ensure();
-    PyEval_CallMethod(callbackFunction, "callback", "(s)", dummy.map_base, NULL);
+    PyEval_CallMethod(callbackFunction, "callback", "(s)", pythonTransport.map_base, NULL);
     PyGILState_Release(gstate);
     return 0;
 }
 static int heard2_cb(struct PortalInternal *p,uint16_t a, uint16_t b) {
-    EchoIndicationJson_heard2 (&dummy, a, b);
+    EchoIndicationJson_heard2 (&pythonTransport, a, b);
     PyGILState_STATE gstate = PyGILState_Ensure();
-    PyEval_CallMethod(callbackFunction, "callback", "(s)", dummy.map_base, NULL);
+    PyEval_CallMethod(callbackFunction, "callback", "(s)", pythonTransport.map_base, NULL);
     PyGILState_Release(gstate);
     return 0;
 }
@@ -78,8 +78,8 @@ void set_callback(PyObject *param)
 {
     Py_INCREF(param);
     callbackFunction = param;
-    dummy.transport = &callbackTransport;
-    dummy.map_base = (volatile unsigned int *)malloc(1000);
+    pythonTransport.transport = &callbackTransport;
+    pythonTransport.map_base = (volatile unsigned int *)malloc(1000);
 }
 
 void *trequest()
@@ -95,7 +95,7 @@ void *tindication()
     init_portal_internal(&eindication, IfcNames_EchoIndicationH2S, DEFAULT_TILE,
 			 (PORTAL_INDFUNC) EchoIndication_handleMessage, &EchoInd_cbTable, NULL, NULL, parent, EchoIndication_reqinfo);
     // encode message as vector ["methodname", arg0, arg1, ...]
-    dummy.json_arg_vector = 1;
+    pythonTransport.json_arg_vector = 1;
     return &eindication;
 }
 } // extern "C"
