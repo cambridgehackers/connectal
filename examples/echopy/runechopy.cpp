8997a56abbf51f85ef6d6a433d1c9c2d73007bcf
Jamey Hicks <jamey.hicks@gmail.com>
2016-05-16 13:18:13 -0400

    use uname to determine OS in runecho.cpp

--- examples/echopy/runechopy.cpp
+++ examples/echopy/runechopy.cpp
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/utsname.h>
 
 int main(int argc, char * const *argv)
 {
     const char *exename = "../testecho.py";
     char library_path[4096];
+    struct utsname utsname;
+
     if (argc > 1)
 	exename = argv[1];
-#if (OS == android)
-    strncpy(library_path, "./bin:../lib:.", sizeof(library_path));
-    exename = "../bin/python";
-#else
-    strncpy(library_path, "./bin", sizeof(library_path));
-#endif
+    uname(&utsname);
+    if (strcmp(utsname.machine, "armv7l") == 0) {
+	strncpy(library_path, "./bin:../lib:.", sizeof(library_path));
+	exename = "../bin/python";
+    } else {
+	strncpy(library_path, "./bin", sizeof(library_path));
+    }
     
     if (getenv("LD_LIBRARY_PATH") != 0) {
         strncat(library_path, ":", sizeof(library_path)-strlen(library_path)-1);
