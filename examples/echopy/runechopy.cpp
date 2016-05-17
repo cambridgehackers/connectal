cbe747d688fbf952d7474dcb8968edeabaa7583d
Jamey Hicks <jamey.hicks@gmail.com>
2016-05-17 15:25:13 -0400

    updated for use via scripts/run.pcie

--- examples/echopy/runechopy.cpp
+++ examples/echopy/runechopy.cpp
@@ -3,21 +3,30 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/utsname.h>
+#include <libgen.h>
 
 int main(int argc, char * const *argv)
 {
     const char *exename = "../testecho.py";
     char library_path[4096];
     struct utsname utsname;
+    const char *libdir = "./bin";
 
-    if (argc > 1)
+    fprintf(stderr, "runecho args: ");
+    for (int i = 0; i < argc; i++)
+      fprintf(stderr, " %s", argv[i]);
+    fprintf(stderr, "\n");
+    if (argc > 1) {
 	exename = argv[1];
+	// What? dirname modifies its argument?
+	libdir = dirname(strdup(argv[1]));
+    }
     uname(&utsname);
     if (strcmp(utsname.machine, "armv7l") == 0) {
 	strncpy(library_path, "./bin:../lib:.", sizeof(library_path));
 	exename = "../bin/python";
     } else {
-	strncpy(library_path, "./bin", sizeof(library_path));
+	strncpy(library_path, libdir, sizeof(library_path));
     }
     
     if (getenv("LD_LIBRARY_PATH") != 0) {
