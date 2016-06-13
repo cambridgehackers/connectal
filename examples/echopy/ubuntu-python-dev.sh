aaab6ccb60a2e425639eeccccaa57014b831287a
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-13 11:14:38 -0400

    remove debian intermediate files

--- examples/echopy/ubuntu-python-dev.sh
+++ examples/echopy/ubuntu-python-dev.sh
@@ -11,3 +11,4 @@
     xzcat data.tar.xz | tar -xvf - )
 sed -i "s|#define _POSIX_C_SOURCE 200112L|/* _POSIX_C_SOURCE defined by features.h*/|" usr/include/arm-linux-gnueabihf/python2.7/pyconfig.h
 sed -i "s|#define _XOPEN_SOURCE 600|/* _XOPEN_SOURCE defined by features.h*/|" usr/include/arm-linux-gnueabihf/python2.7/pyconfig.h
+rm -f data.tar.xz control.tar.gz debian-binary
