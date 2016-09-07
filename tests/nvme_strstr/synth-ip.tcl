82038869bb782c0d96654f80f8da60e6b0f71fcc
Jamey Hicks <jamey.hicks@gmail.com>
2016-09-07 14:28:59 -0400

    use same datawidth for pcie3 for now

--- tests/nvme_strstr/synth-ip.tcl
+++ tests/nvme_strstr/synth-ip.tcl
@@ -2,19 +2,19 @@ source "board.tcl"
 
 if {$boardname == {nfsume}} {
     set partname {xc7vx690tffg1761-2}
-    set databuswidth 256
+    set databuswidth 128
 }
 if {$boardname == {vc709}} {
     set partname {xc7vx690tffg1761-2}
-    set databuswidth 256
+    set databuswidth 128
 }
 if {$boardname == {miniitx100}} {
     set partname {xc7z100ffg900-2}
-    set databuswidth 64
+    set databuswidth 128
 }
 if {$boardname == {zc706}} {
     set partname {xc7z045ffg900-2}
-    set databuswidth 64
+    set databuswidth 128
 }
 puts "partname=$partname"
 puts "databuswidth=$databuswidth"
@@ -91,6 +91,6 @@ proc fpgamake_ipcore {core_name core_version ip_name params} {
 if {$partname != {xc7vx690tffg1761-2}} {
     fpgamake_ipcore axi_pcie 2.8 axi_pcie_rp [list CONFIG.INCLUDE_RC {Root_Port_of_PCI_Express_Root_Complex} CONFIG.NO_OF_LANES {X4} CONFIG.BAR0_SCALE {Gigabytes} CONFIG.INCLUDE_BAROFFSET_REG {false} CONFIG.AXIBAR_0 {0x00000000} CONFIG.AXIBAR_HIGHADDR_0 {0xfFFFFFFF} CONFIG.AXIBAR2PCIEBAR_0 {0x00000000} CONFIG.BASEADDR {0x00000000} CONFIG.HIGHADDR {0xffffffff} CONFIG.XLNX_REF_BOARD {ZC706} CONFIG.shared_logic_in_core {true} CONFIG.MAX_LINK_SPEED {5.0_GT/s} CONFIG.DEVICE_ID {0x7022} CONFIG.BASE_CLASS_MENU {Bridge_device} CONFIG.SUB_CLASS_INTERFACE_MENU {InfiniBand_to_PCI_host_bridge} CONFIG.BAR0_SIZE {1} CONFIG.S_AXI_DATA_WIDTH {128} CONFIG.M_AXI_DATA_WIDTH {128} CONFIG.NUM_MSI_REQ {5} CONFIG.S_AXI_SUPPORTS_NARROW_BURST {true}]
 } else {
-    fpgamake_ipcore axi_pcie3 2.1 axi_pcie_rp [list CONFIG.mode_selection {Advanced} CONFIG.device_port_type {Root_Port_of_PCI_Express_Root_Complex} CONFIG.pcie_blk_locn {X0Y0} CONFIG.pl_link_cap_max_link_speed {8.0_GT/s} CONFIG.SYS_RST_N_BOARD_INTERFACE {Custom} CONFIG.PCIE_BOARD_INTERFACE {Custom} CONFIG.pl_link_cap_max_link_width {X8} CONFIG.axi_data_width {256_bit} CONFIG.plltype {QPLL1} CONFIG.axisten_freq {250} CONFIG.dedicate_perst {false} CONFIG.pf0_device_id {7131} CONFIG.pf0_base_class_menu {Bridge_device} CONFIG.pf0_class_code_base {06} CONFIG.pf0_sub_class_interface_menu {CardBus_bridge} CONFIG.pf0_class_code_sub {07} CONFIG.pf0_class_code {060700} CONFIG.pf0_bar0_size {2} CONFIG.pf0_bar0_scale {Gigabytes} CONFIG.axibar_highaddr_0 {0xffffffffffffffff} ]
+    fpgamake_ipcore axi_pcie3 2.1 axi_pcie_rp [list CONFIG.mode_selection {Advanced} CONFIG.device_port_type {Root_Port_of_PCI_Express_Root_Complex} CONFIG.pcie_blk_locn {X0Y0} CONFIG.pl_link_cap_max_link_speed {5.0_GT/s} CONFIG.SYS_RST_N_BOARD_INTERFACE {Custom} CONFIG.PCIE_BOARD_INTERFACE {Custom} CONFIG.pl_link_cap_max_link_width {X8} CONFIG.axi_data_width {128_bit} CONFIG.plltype {CPLL} CONFIG.axisten_freq {250} CONFIG.dedicate_perst {false} CONFIG.pf0_device_id {7131} CONFIG.pf0_base_class_menu {Bridge_device} CONFIG.pf0_class_code_base {06} CONFIG.pf0_sub_class_interface_menu {CardBus_bridge} CONFIG.pf0_class_code_sub {07} CONFIG.pf0_class_code {060700} CONFIG.pf0_bar0_size {2} CONFIG.pf0_bar0_scale {Gigabytes} CONFIG.axibar_highaddr_0 {0xffffffffffffffff} ]
 }
 
