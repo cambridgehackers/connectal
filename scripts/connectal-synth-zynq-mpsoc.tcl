30c635e92180722785d6f12fb734b712f070c0cd
Chanwoo Chung <cwchung@mit.edu>
2017-03-09 16:25:59 -0500

    zynq-mpsoc: 200MHz pl_clk0, pl_clk1

--- scripts/connectal-synth-zynq-mpsoc.tcl
+++ scripts/connectal-synth-zynq-mpsoc.tcl
@@ -3,4 +3,4 @@ source "$connectaldir/scripts/connectal-synth-ip.tcl"
 
 set prj_boardname $boardname
 
-connectal_synth_ip zynq_ultra_ps_e 2.0 zynq_ultra_ps_e_0 [list CONFIG.PSU__FPGA_PL1_ENABLE {1} CONFIG.PSU__USE__M_AXI_GP0 {1} CONFIG.PSU__USE__IRQ0 {1} CONFIG.PSU__CRL_APB__PL1_REF_CTRL__FREQMHZ {200} CONFIG.PSU__USE__S_AXI_GP0 {1}]
+connectal_synth_ip zynq_ultra_ps_e 2.0 zynq_ultra_ps_e_0 [list CONFIG.PSU__FPGA_PL1_ENABLE {1} CONFIG.PSU__USE__M_AXI_GP0 {1} CONFIG.PSU__USE__IRQ0 {1} CONFIG.PSU__CRL_APB__PL0_REF_CTRL__FREQMHZ {200} CONFIG.PSU__CRL_APB__PL1_REF_CTRL__FREQMHZ {200} CONFIG.PSU__USE__S_AXI_GP0 {1}]
