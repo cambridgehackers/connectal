8fd2cb7d65c3be177d668d286f81edd9daa7edf9
Jamey Hicks <jamey.hicks@gmail.com>
2017-09-13 18:57:58 +0000

    add setup actions to scripts/aws/build.sh

--- scripts/aws/build.sh
+++ scripts/aws/build.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-HERE=$PWD
-cd ../..
+SCRIPTSDIR=`pwd`
+echo SCRIPTSDIR=$SCRIPTSDIR
+if [ `basename $SCRIPTSDIR` = 'scripts' ]; then
+  cd ../..
+else
+  mkdir -p build/checkpoints/to_aws
+  mkdir -p build/constraints
+  mkdir -p build/reports
+  mkdir -p build/scripts
+  mkdir -p build/src_post_encryption
+  SCRIPTSDIR=`pwd`/build/scripts
+  echo SCRIPTSDIR=$SCRIPTSDIR
+fi
+
+rsync -v $CONNECTALDIR/scripts/aws/* $SCRIPTSDIR
+
 export CL_DIR=`pwd`
-cd $HERE
+cd $SCRIPTSDIR
 
 pushd ~/aws-fpga
 source ./hdk_setup.sh
 popd
 
+echo '#placeholder' > ../constraints/cl_pnr_user.xdc
+echo '#placeholder' > ../constraints/cl_synth_user.xdc
 
 ~/aws-fpga/hdk/cl/examples/cl_hello_world/build/scripts/aws_build_dcp_from_cl.sh -ignore_memory_requirement -notify
