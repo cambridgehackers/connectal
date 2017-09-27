7baceefca97073b6cc7cc7a259df7cf5cae1b881
Jamey Hicks <jamey.hicks@gmail.com>
2017-09-27 18:35:49 +0000

    also upload debug_probes.ltx to S3

--- scripts/aws/upload.sh
+++ scripts/aws/upload.sh
@@ -6,6 +6,10 @@ if [ "$filename" = "" ]; then
     exit 1
 fi
 
+basename=`basename $filename .Developer_CL.tar`
+echo "basename=$basename"
+
 aws s3 cp ../checkpoints/to_aws/$filename s3://aws-fpga/simple/$filename
+aws s3 cp ../checkpoints.$basename.debug_probes.ltx s3://aws-fpga/simple/$filename
 aws ec2 create-fpga-image --name simple --description "$filename" --input-storage-location Bucket=aws-fpga,Key=simple/$filename --logs-storage-location Bucket=aws-fpga,Key=logs-folder
 aws ec2 describe-fpga-images
