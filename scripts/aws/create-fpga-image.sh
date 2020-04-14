3429f17bc26cba6b3ca33de2e9631d9b9ae713cb
Jamey Hicks <jamey.hicks@accelerated.tech>
2020-04-14 17:56:48 +0000

    enable create-fpga-image.sh to be run from either design or awsf1 directory

--- scripts/aws/create-fpga-image.sh
+++ scripts/aws/create-fpga-image.sh
@@ -8,6 +8,13 @@ if [ "$name" == "" -o "$timestamp" == "" ]; then
     exit -1
 fi
 
-aws s3 cp awsf1/build/checkpoints/to_aws/$timestamp.Developer_CL.tar s3://aws-fpga/$name/
-aws s3 cp awsf1/build/checkpoints/$timestamp.debug_probes.ltx s3://aws-fpga/$name/
+if [ -d "build/checkpoints" ]; then
+    CHECKPOINTS_DIR="build/checkpoints"
+fi
+if [ -d "awsf1/build/checkpoints" ]; then
+    CHECKPOINTS_DIR="awsf1/build/checkpoints"
+fi
+
+aws s3 cp $CHECKPOINTS_DIR/to_aws/$timestamp.Developer_CL.tar s3://aws-fpga/$name/
+aws s3 cp $CHECKPOINTS_DIR/$timestamp.debug_probes.ltx s3://aws-fpga/$name/
 aws ec2 create-fpga-image --name $name --description $timestamp --input-storage-location Bucket=aws-fpga,Key=$name/$timestamp.Developer_CL.tar --logs-storage-location Bucket=aws-fpga,Key=logs-folder | tee latest-fpga-image.json
