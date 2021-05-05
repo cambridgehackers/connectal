702f10b30a49ff139485e8350f03d923d803e703
John Ankcorn <jca@alum.mit.edu>
2021-05-04 18:12:31 -0700

    added build arch target for vcs (previously, had limited support)

--- cpp/portal.c
+++ cpp/portal.c
@@ -286,14 +286,14 @@ static void initPortalHardwareOnce(void)
         argv[ind++] = (char *)"work.xsimtop";
 #endif
 #if defined(BOARD_vcs)
-#if defined(__ATOMICC__)
 	const char *exetype = "simv";
         argv[ind++] = (char *)"+verbose=1";
-#else
-	const char *exetype = "vcs";
-	bindir = 0; // the simulation driver is found in $PATH
-	//FIXME ARGS
-#endif
+        argv[ind++] = (char *)"-assert";
+        argv[ind++] = (char *)"verbose+success";
+	if (simulator_dump_vcd) {
+	  //argv[ind++] = (char*)"-t";
+	  //argv[ind++] = (char*)simulator_vcd_name;
+	}
 #endif
 #if defined(BOARD_vsim)
 	const char *exetype = "vsim";
