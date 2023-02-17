4f55499fed5ec87eee098fe98388738e9d280bc1
Jamey Hicks <jamey.hicks@accelerated.tech>
2023-02-17 17:51:02 -0500

    more zynq ultrascale refactoring

--- scripts/connectal-synth-zynq-mpsoc.tcl
+++ scripts/connectal-synth-zynq-mpsoc.tcl
@@ -3,7 +3,20 @@ source "$connectaldir/scripts/connectal-synth-ip.tcl"
 
 set prj_boardname $boardname
 
-connectal_synth_ip zynq_ultra_ps_e 2.0 zynq_ultra_ps_e_0 [list \
+if {"$XILINX" == "1"} {
+    set zynq_ultra_ps_version 2.0
+    if {[version -short] == "2022.2"} {
+        set zynq_ultra_ps_version {3.4}
+    }
+    if {[version -short] == "2022.1"} {
+        set zynq_ultra_ps_version {3.4}
+    }
+    if {[version -short] == "2019.1"} {
+        set zynq_ultra_ps_version {3.3}
+    }
+}
+
+connectal_synth_ip zynq_ultra_ps_e ${zynq_ultra_ps_version} zynq_ultra_ps_e_0 [list \
 							  CONFIG.PSU__FPGA_PL1_ENABLE {1} \
 							  CONFIG.PSU__USE__M_AXI_GP0 {1} \
 							  CONFIG.PSU__USE__IRQ0 {1} \
